75c164490236a9355a2278dd20f50dc84fd5a3a0
Robert Klotzner <robert.klotzner@gmx.at>
2016-07-04 08:27:57 +0200

    Implemented more ToHttpApiData/FromHttpApiData functions

--- src/Servant/PureScript.hs
+++ src/Servant/PureScript.hs
@@ -21,25 +21,30 @@ module Servant.PureScript (
 , defaultSettings
 , addReaderParam
 , jsonParseUrlPiece
+, jsonToUrlPiece
+, jsonParseHeader
+, jsonToHeader
 ) where
 
 
 import           Data.Aeson
 import           Data.Bifunctor
+import           Data.ByteString             (ByteString)
+import qualified Data.ByteString.Lazy        as BS
 import           Data.Monoid
 import           Data.Proxy
-import           Data.Text                           (Text)
-import qualified Data.Text                           as T
-import qualified Data.Text.Encoding                  as T
+import           Data.Text                   (Text)
+import qualified Data.Text                   as T
+import qualified Data.Text.Encoding          as T
 import           Language.PureScript.Bridge
-import           Network.HTTP.Types                  (urlDecode)
+import           Network.HTTP.Types          (urlDecode, urlEncode)
 import           Servant.Foreign
 import           Servant.PureScript.CodeGen
 import           Servant.PureScript.Internal
 import           System.Directory
 import           System.FilePath
-import           System.IO                           (IOMode (..), withFile)
-import           Text.PrettyPrint.Mainland           (hPutDocLn)
+import           System.IO                   (IOMode (..), withFile)
+import           Text.PrettyPrint.Mainland   (hPutDocLn)
 
 writeAPIModule :: forall bridgeSelector api.
   ( HasForeign (PureScript bridgeSelector) PSType api
@@ -64,7 +69,22 @@ writeAPIModuleWithSettings opts root pBr pAPI = do
     mDir = takeDirectory mPath
 
 
--- | Use this function for implementing 'parseURLPiece' in your FromHttpApiData instances
+-- | Use this function for implementing 'parseUrlPiece' in your FromHttpApiData instances
 --   in order to be compatible with the generated PS code.
 jsonParseUrlPiece :: FromJSON a => Text -> Either Text a
-jsonParseUrlPiece = first T.pack . eitherDecodeStrict . urlDecode True . T.encodeUtf8
+jsonParseUrlPiece = jsonParseHeader . T.encodeUtf8
+
+-- | Use this function for implementing 'toUrlPiece' in your ToHttpApiData instances
+--   in order to be compatible with the generated PS code.
+jsonToUrlPiece :: ToJSON a => a -> Text
+jsonToUrlPiece = T.decodeUtf8 . jsonToHeader
+
+-- | Use this function for implementing 'parseHeader' in your FromHttpApiData instances
+--   in order to be compatible with the generated PS code.
+jsonParseHeader :: FromJSON a => ByteString -> Either Text a
+jsonParseHeader = first T.pack . eitherDecodeStrict . urlDecode True
+
+-- | Use this function for implementing 'toHeader' in your ToHttpApiData instances
+--   in order to be compatible with the generated PS code.
+jsonToHeader :: ToJSON a => a -> ByteString
+jsonToHeader = urlEncode True . BS.toStrict . encode
