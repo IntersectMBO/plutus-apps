1dcc835ea4c1d4e5697a0f92509eacd70a52712d
Robert Klotzner <robert.klotzner@gmx.at>
2016-08-22 07:47:48 +0200

    Made stack.nix actually work
    Bumped version

--- stack.nix
+++ stack.nix
@@ -1,12 +1,12 @@
-{ nixpkgs ? import <nixpkgs> {}, ghc }:
-    let
-        buildStackProject = nixpkgs.haskell.lib.buildStackProject.override { inherit ghc;};
-    in
-        with nixpkgs;
-
-        buildStackProject {
-            name = builtins.trace ghc.name "servant-subscriber-env";
-            # ncurses needed for intero
-            buildInputs = [ gcc haskell.packages.lts-6_0.ghc-mod git ncurses zlib ];
-            shellHook = "export SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt";
-        }
+with (import <nixpkgs> {});
+let
+  haskellPackages = haskell.packages.lts-6_7;
+  ghc = haskellPackages.ghc;
+in
+  haskell.lib.buildStackProject {
+  name = "myEnv";
+  #    buildInputs = [ gcc git zlib pkgconfig ghc glibcLocales ];
+  buildInputs = [ zlib haskellPackages.ghc-mod postgresql ];
+    ghc = ghc;
+    shellHook = "export SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt";
+}
