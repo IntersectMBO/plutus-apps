eda6b08a2477cb0cab7c2c4d9565c531591e5362
Robert Klotzner <robert.klotzner@gmx.at>
2016-08-26 14:52:18 +0200

    Made central-counter example work with typed-inteface
    branch of purescript-subscriber

--- examples/central-counter/frontend/src/Main.purs
+++ examples/central-counter/frontend/src/Main.purs
@@ -10,6 +10,7 @@ import Control.Monad.Reader.Trans
 import Counter.ServerTypes
 import Counter.WebAPI
 import DOM.Node.Node (baseURI)
+import Data.Argonaut.Core (Json)
 import Data.Argonaut.Generic.Aeson
 import Data.Argonaut.Parser (jsonParser)
 import Data.Array as Array
@@ -27,14 +28,14 @@ import Pux.Html (Html, text, button, span, div, p)
 import Pux.Html.Events (onClick)
 import Servant.PureScript.Affjax
 import Servant.PureScript.Settings
-import Servant.Subscriber (SubscriberEff, Notification(WebSocketClosed, WebSocketError, Modified), makeSubscriber, Subscriber, Config)
 import Servant.Subscriber.Request (HttpRequest(..))
-import Servant.Subscriber as Subscriber
 import Servant.Subscriber.Types (Path(Path))
 import Signal (Signal)
 import Signal.Channel (Channel, subscribe, send, channel, CHANNEL)
 import Unsafe.Coerce (unsafeCoerce)
 import WebSocket (WEBSOCKET)
+import Servant.Subscriber (Subscriber, makeSubscriber, SubscriberEff, Config, makeSubscriptions)
+import Servant.Subscriber as Subscriber
 
 
 
@@ -43,6 +44,7 @@ data Action = Increment
             | Update Int
             | ReportError AjaxError
             | SubscriberLog String
+            | Nop
 
 type State = {
     counter :: Int
@@ -77,6 +79,7 @@ update Decrement state = runEffectActions state [Update <$> putCounter (CounterA
 update (Update val) state  = { state : state { counter = val }, effects : []}
 update (ReportError err ) state = { state : state { lastError = Just err}, effects : []}
 update (SubscriberLog msg) state = { state : state { subscriberLog = Cons msg state.subscriberLog}, effects : []}
+update Nop state = noEffects state
 
 view :: State -> Html Action
 view state =
@@ -107,37 +110,38 @@ runEffect settings m = do
       Left err -> pure $ ReportError err
       Right v -> pure v
 
-type SubscriberData = {
-  subscriber :: Subscriber
-, messages :: Signal (Maybe Notification)
+type SubscriberData eff = {
+  subscriber :: Subscriber eff Action
+, messages :: Signal Action
 }
 
 
-initSubscriber :: forall eff. SubscriberEff (channel :: CHANNEL | eff) SubscriberData
+initSubscriber :: forall eff. SubscriberEff (channel :: CHANNEL | eff) (SubscriberData (channel :: CHANNEL | eff))
 initSubscriber = do
-  ch <- coerceEffects $ channel (Nothing :: Maybe Notification)
+  ch <- channel Nop
   let
-    c :: Config
+    c :: Config (channel :: CHANNEL | eff) Action
     c = {
         url : "ws://localhost:8081/subscriber"
-      , notify : coerceEffects <<< send ch <<< Just
+      , notify : send ch <<< SubscriberLog <<< gShow
+      , callback : send ch
       }
   sub <- makeSubscriber c
   let sig = subscribe ch
-  Subscriber.subscribe subscriberRequest sub
+  let subs = makeSubscriptions subscriberRequest handleResponse
+  Subscriber.deploy subs sub
   pure $ { subscriber : sub, messages : sig }
 
 
-toAction :: Maybe Notification -> Action
-toAction Nothing                 = SubscriberLog "Received Nothing"
-toAction (Just notification)     = case notification of
-  Modified path val -> handleModify val
-  _ -> SubscriberLog $ gShow notification
+-- toAction :: Maybe Notification -> Action
+-- toAction Nothing                 = SubscriberLog "Received Nothing"
+-- toAction (Just notification)     = case notification of
+--   Modified path val -> handleModify val
+--   _ -> SubscriberLog $ gShow notification
 
-handleModify :: String -> Action
-handleModify msg = case decodeJson <=< jsonParser $ msg of
-                      Left err -> SubscriberLog $ "Could not parse response body: " <> err
-                      Right v -> Update v
+handleResponse :: Maybe Json -> Either String Action
+handleResponse Nothing = pure Nop
+handleResponse (Just v) = map Update <<< decodeJson $ v
 
 
 -- main :: forall e. Eff (ajax :: AJAX, err :: EXCEPTION, channel :: CHANNEL | e) Unit
@@ -158,7 +162,7 @@ main = do
     { initialState: initState
     , update: update
     , view: view
-    , inputs: [map toAction sub.messages]
+    , inputs: [sub.messages]
     --, inputs : []
     }
 
