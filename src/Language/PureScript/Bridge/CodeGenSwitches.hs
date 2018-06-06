c7b0af7aefec2bd7c2eebc1ce2fa89f0cc37d648
Carsten Koenig <Carsten@gettingsharper.de>
2018-06-06 07:16:06 +0200

    added switches for legacy purs11 and generics-rep

--- src/Language/PureScript/Bridge/CodeGenSwitches.hs
+++ src/Language/PureScript/Bridge/CodeGenSwitches.hs
@@ -2,25 +2,32 @@
 module Language.PureScript.Bridge.CodeGenSwitches 
     ( Settings (..)
     , defaultSettings
+    , purs_0_11_settings
     , Switch
     , getSettings
     , defaultSwitch
     , noLenses, genLenses
+    , useGen, useGenRep
     ) where
 
 
 import Data.Monoid (Endo(..))
 
 -- |general settings for code generation
-newtype Settings = Settings
+data Settings = Settings
     { generateLenses :: Bool -- ^use purescript-profunctor-lens for generated PS-types?
+    , genericsGenRep :: Bool -- ^generate generics using purescript-generics-rep instead of purescript-generics
     }
     deriving (Eq, Show)
 
 
 -- |Settings to generate Lenses
 defaultSettings :: Settings
-defaultSettings = Settings True
+defaultSettings = Settings True True
+
+-- |settings for purescript 0.11.x
+purs_0_11_settings :: Settings
+purs_0_11_settings = Settings True False
 
 
 -- |you can `mappend` switches to control the code generation
@@ -45,3 +52,14 @@ noLenses = Endo $ \settings -> settings { generateLenses = False }
 -- |switch on the generatation of profunctor-lenses
 genLenses :: Switch
 genLenses = Endo $ \settings -> settings { generateLenses = True }
+
+
+-- |generate generics using purescript-generics-rep
+useGenRep :: Switch
+useGenRep = Endo $ \settings -> settings { genericsGenRep = True }
+
+
+-- |generate generics using purescript-generics
+useGen :: Switch
+useGen = Endo $ \settings -> settings { genericsGenRep = False }
+
