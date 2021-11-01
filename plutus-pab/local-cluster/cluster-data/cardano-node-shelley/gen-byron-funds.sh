16de0ce5674f4d4f9272f984adab6bf6d185454c
Jann Müller <j-mueller@users.noreply.github.com>
2021-11-01 10:21:07 +0100

    fix shellcheck

--- plutus-pab/local-cluster/cluster-data/cardano-node-shelley/gen-byron-funds.sh
+++ plutus-pab/local-cluster/cluster-data/cardano-node-shelley/gen-byron-funds.sh
@@ -8,22 +8,22 @@ set -euo pipefail
 # Generates byron key file, shelley key file, address.
 # The addresses can be inserted to the byron genesis.
 # The corresponding TxIn will then be blake2d256 of the address.
-cd `dirname $0`
+cd "$(dirname $"0")"
 dir=faucet-addrs
 rm -f $dir/*
 for i in $(seq 200);
 do
   # Generate payment key and addresses for payment and genesis utxo
-  cardano-cli byron key keygen --secret $dir/faucet$i.byron.key
+  cardano-cli byron key keygen --secret $dir/faucet"$i".byron.key
   cardano-cli \
     byron key signing-key-address \
     --byron-formats \
     --mainnet \
-    --secret $dir/faucet$i.byron.key \
-    | head -n 1 > $dir/faucet$i.addr
+    --secret $dir/faucet"$i".byron.key \
+    | head -n 1 > $dir/faucet"$i".addr
 
   cardano-cli key convert-byron-key \
     --byron-payment-key-type \
-    --byron-signing-key-file $dir/faucet$i.byron.key \
-    --out-file $dir/faucet$i.shelley.key
+    --byron-signing-key-file $dir/faucet"$i".byron.key \
+    --out-file $dir/faucet"$i".shelley.key
 done
--- plutus-pab/local-cluster/cluster-data/cardano-node-shelley/start.sh
+++ plutus-pab/local-cluster/cluster-data/cardano-node-shelley/start.sh
@@ -3,20 +3,20 @@
 set -euo pipefail
 
 if type -p gdate > /dev/null; then
-  gnu_date=gdate
+  gnu_date="gdate"
 else
-  gnu_date=date
+  gnu_date="date"
 fi
 
 systemStart=$($gnu_date --iso-8601=s --date="5 seconds")
 
 config_dir=lib/shelley/test/data/cardano-node-shelley
 
-mkdir -p ${state_dir:=bft-node}
+mkdir -p "${state_dir:=bft-node}"
 
 yq -y '. + { GenesisFile: "genesis.json", minSeverity: "Info" }' < $config_dir/node.config > $state_dir/node.config
 
-yq ".systemStart=\"$(date --iso-8601=s --date='5 seconds')\"" < $config_dir/genesis.yaml > $state_dir/genesis.json
+yq ".systemStart=\"$systemStart\"" < $config_dir/genesis.yaml > $state_dir/genesis.json
 
 set -x
 
