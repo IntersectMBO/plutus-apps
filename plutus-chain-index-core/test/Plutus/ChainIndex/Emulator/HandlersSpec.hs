e7f560c2b7cf631a5b6c068fc7f00c0903ead348
hainq <hai@meld.com>
2021-11-13 17:53:26 +0700

    plutus-chain-index-core: add BlockProcessOption tests. Tighten and start sharing code among existing tests as well.

--- plutus-chain-index-core/test/Plutus/ChainIndex/Emulator/HandlersSpec.hs
+++ plutus-chain-index-core/test/Plutus/ChainIndex/Emulator/HandlersSpec.hs
@@ -8,7 +8,7 @@
 module Plutus.ChainIndex.Emulator.HandlersSpec (tests) where
 
 import Control.Lens
-import Control.Monad (forM, forM_)
+import Control.Monad (forM)
 import Control.Monad.Freer (Eff, interpret, reinterpret, runM)
 import Control.Monad.Freer.Error (Error, runError)
 import Control.Monad.Freer.Extras (LogMessage, LogMsg (..), handleLogWriter)
@@ -17,20 +17,22 @@ import Control.Monad.Freer.Writer (runWriter)
 import Control.Monad.IO.Class (liftIO)
 import Data.Default (def)
 import Data.Sequence (Seq)
-import Data.Set (member)
+import Data.Set qualified as S
 import Generators qualified as Gen
-import Ledger (Address (Address, addressCredential), TxOut (TxOut, txOutAddress), outValue)
+import Ledger (outValue)
 import Plutus.ChainIndex (ChainIndexLog, Page (pageItems), PageQuery (PageQuery), appendBlock, txFromTxId,
-                          utxoSetAtAddress, utxoSetWithCurrency)
+                          utxoSetMembership, utxoSetWithCurrency)
 import Plutus.ChainIndex.ChainIndexError (ChainIndexError)
 import Plutus.ChainIndex.Effects (ChainIndexControlEffect, ChainIndexQueryEffect)
 import Plutus.ChainIndex.Emulator.Handlers (ChainIndexEmulatorState, handleControl, handleQuery)
 import Plutus.ChainIndex.Tx (_ValidTx, citxOutputs, citxTxId)
+import Plutus.ChainIndex.Types (BlockProcessOption (..))
 import Plutus.V1.Ledger.Value (AssetClass (AssetClass), flattenValue)
 
 import Hedgehog (Property, assert, forAll, property, (===))
 import Test.Tasty
 import Test.Tasty.Hedgehog (testProperty)
+import Util (utxoSetFromBlockAddrs)
 
 tests :: TestTree
 tests = do
@@ -44,6 +46,9 @@ tests = do
     , testGroup "utxoSetWithCurrency"
       [ testProperty "each txOutRef should be unspent" eachTxOutRefWithCurrencyShouldBeUnspentSpec
       , testProperty "should restrict to non-ADA currencies" cantRequestForTxOutRefsWithAdaSpec
+      ]
+    , testGroup "BlockProcessOption"
+      [ testProperty "do not store txs" doNotStoreTxs
       ]
     ]
 
@@ -70,24 +75,14 @@ eachTxOutRefAtAddressShouldBeUnspentSpec :: Property
 eachTxOutRefAtAddressShouldBeUnspentSpec = property $ do
   ((tip, block), state) <- forAll $ Gen.runTxGenState Gen.genNonEmptyBlock
 
-  let addresses =
-        fmap (\TxOut { txOutAddress = Address { addressCredential }} -> addressCredential)
-        $ view (traverse . citxOutputs . _ValidTx) block
-
   result <- liftIO $ runEmulatedChainIndex mempty $ do
     -- Append the generated block in the chain index
     appendBlock tip block def
-
-    forM addresses $ \addr -> do
-      let pq = PageQuery 200 Nothing
-      (_, utxoRefs) <- utxoSetAtAddress pq addr
-      pure $ pageItems utxoRefs
+    utxoSetFromBlockAddrs block
 
   case result of
-    Left _ -> Hedgehog.assert False
-    Right utxoRefsGroups -> do
-      forM_ (concat utxoRefsGroups) $ \utxoRef -> do
-        Hedgehog.assert $ utxoRef `member` view Gen.txgsUtxoSet state
+    Left _           -> Hedgehog.assert False
+    Right utxoGroups -> S.fromList (concat utxoGroups) === view Gen.txgsUtxoSet state
 
 -- | After generating and appending a block in the chain index, verify that
 -- querying the chain index with each of the asset classes in the block returns
@@ -111,11 +106,8 @@ eachTxOutRefWithCurrencyShouldBeUnspentSpec = property $ do
       pure $ pageItems utxoRefs
 
   case result of
-    Left _ -> Hedgehog.assert False
-    Right utxoRefsGroups -> do
-      let utxoRefs = concat utxoRefsGroups
-      forM_ utxoRefs $ \utxoRef -> do
-        Hedgehog.assert $ utxoRef `member` view Gen.txgsUtxoSet state
+    Left _           -> Hedgehog.assert False
+    Right utxoGroups -> S.fromList (concat utxoGroups) === view Gen.txgsUtxoSet state
 
 -- | Requesting UTXOs containing Ada should not return anything because every
 -- transaction output must have a minimum of 1 ADA. So asking for UTXOs with ADA
@@ -136,6 +128,23 @@ cantRequestForTxOutRefsWithAdaSpec = property $ do
     Left _         -> Hedgehog.assert False
     Right utxoRefs -> Hedgehog.assert $ null utxoRefs
 
+-- | Do not store txs through BlockProcessOption.
+-- The UTxO set must still be stored.
+-- But cannot be fetched through addresses as addresses are not stored.
+doNotStoreTxs :: Property
+doNotStoreTxs = property $ do
+  ((tip, block), state) <- forAll $ Gen.runTxGenState Gen.genNonEmptyBlock
+  result <- liftIO $ runEmulatedChainIndex mempty $ do
+    appendBlock tip block BlockProcessOption{bpoStoreTxs=False}
+    tx <- txFromTxId (view citxTxId (head block))
+    utxosFromAddr <- utxoSetFromBlockAddrs block
+    utxosStored <- traverse utxoSetMembership (S.toList (view Gen.txgsUtxoSet state))
+    pure (tx, concat utxosFromAddr, utxosStored)
+  case result of
+    Right (Nothing, [], utxosStored) -> Hedgehog.assert $ and (snd <$> utxosStored)
+    _                                -> Hedgehog.assert False
+
+-- | Run an emulated chain index effect against a starting state
 runEmulatedChainIndex
   :: ChainIndexEmulatorState
   -> Eff '[ ChainIndexControlEffect
--- plutus-chain-index-core/test/Plutus/ChainIndex/HandlersSpec.hs
+++ plutus-chain-index-core/test/Plutus/ChainIndex/HandlersSpec.hs
@@ -8,31 +8,33 @@
 module Plutus.ChainIndex.HandlersSpec (tests) where
 
 import Control.Concurrent.STM (newTVarIO)
-import Control.Lens
-import Control.Monad (forM, forM_)
+import Control.Lens (view)
+import Control.Monad (forM)
 import Control.Monad.Freer (Eff)
 import Control.Monad.Freer.Extras.Beam (BeamEffect)
 import Control.Monad.IO.Class (liftIO)
 import Control.Tracer (nullTracer)
 import Data.Default (def)
-import Data.Set (member)
+import Data.Set qualified as S
 import Database.Beam.Migrate.Simple (autoMigrate)
 import Database.Beam.Sqlite qualified as Sqlite
 import Database.Beam.Sqlite.Migrate qualified as Sqlite
 import Database.SQLite.Simple qualified as Sqlite
 import Generators qualified as Gen
 import Hedgehog (Property, assert, forAll, property, (===))
-import Ledger (Address (Address, addressCredential), TxOut (TxOut, txOutAddress), outValue)
+import Ledger (outValue)
 import Plutus.ChainIndex (Page (pageItems), PageQuery (PageQuery), RunRequirements (..), appendBlock, citxOutputs,
-                          runChainIndexEffects, txFromTxId, utxoSetAtAddress, utxoSetWithCurrency)
+                          runChainIndexEffects, txFromTxId, utxoSetMembership, utxoSetWithCurrency)
 import Plutus.ChainIndex.ChainIndexError (ChainIndexError (..))
 import Plutus.ChainIndex.DbSchema (checkedSqliteDb)
 import Plutus.ChainIndex.Effects (ChainIndexControlEffect, ChainIndexQueryEffect)
 import Plutus.ChainIndex.Tx (_ValidTx, citxTxId)
+import Plutus.ChainIndex.Types (BlockProcessOption (..))
 import Plutus.V1.Ledger.Ada qualified as Ada
 import Plutus.V1.Ledger.Value (AssetClass (AssetClass), flattenValue)
 import Test.Tasty
 import Test.Tasty.Hedgehog (testProperty)
+import Util (utxoSetFromBlockAddrs)
 
 tests :: TestTree
 tests = do
@@ -46,6 +48,9 @@ tests = do
     , testGroup "utxoSetWithCurrency"
       [ testProperty "each txOutRef should be unspent" eachTxOutRefWithCurrencyShouldBeUnspentSpec
       , testProperty "should restrict to non-ADA currencies" cantRequestForTxOutRefsWithAdaSpec
+      ]
+    , testGroup "BlockProcessOption"
+      [ testProperty "do not store txs" doNotStoreTxs
       ]
     ]
 
@@ -74,26 +79,16 @@ eachTxOutRefAtAddressShouldBeUnspentSpec :: Property
 eachTxOutRefAtAddressShouldBeUnspentSpec = property $ do
   ((tip, block), state) <- forAll $ Gen.runTxGenState Gen.genNonEmptyBlock
 
-  let addresses =
-        fmap (\TxOut { txOutAddress = Address { addressCredential }} -> addressCredential)
-        $ view (traverse . citxOutputs . _ValidTx) block
-
   result <- liftIO $ Sqlite.withConnection ":memory:" $ \conn -> do
     Sqlite.runBeamSqlite conn $ autoMigrate Sqlite.migrationBackend checkedSqliteDb
     liftIO $ runChainIndex conn $ do
       -- Append the generated block in the chain index
       appendBlock tip block def
-
-      forM addresses $ \addr -> do
-        let pq = PageQuery 200 Nothing
-        (_, utxoRefs) <- utxoSetAtAddress pq addr
-        pure $ pageItems utxoRefs
+      utxoSetFromBlockAddrs block
 
   case result of
-    Left _ -> Hedgehog.assert False
-    Right utxoRefsGroups -> do
-      forM_ (concat utxoRefsGroups) $ \utxoRef -> do
-        Hedgehog.assert $ utxoRef `member` view Gen.txgsUtxoSet state
+    Left _           -> Hedgehog.assert False
+    Right utxoGroups -> S.fromList (concat utxoGroups) === view Gen.txgsUtxoSet state
 
 -- | After generating and appending a block in the chain index, verify that
 -- querying the chain index with each of the addresses in the block returns
@@ -120,10 +115,8 @@ eachTxOutRefWithCurrencyShouldBeUnspentSpec = property $ do
         pure $ pageItems utxoRefs
 
   case result of
-    Left _ -> Hedgehog.assert False
-    Right utxoRefsGroups -> do
-      forM_ (concat utxoRefsGroups) $ \utxoRef ->
-        Hedgehog.assert $ utxoRef `member` view Gen.txgsUtxoSet state
+    Left _           -> Hedgehog.assert False
+    Right utxoGroups -> S.fromList (concat utxoGroups) === view Gen.txgsUtxoSet state
 
 -- | Requesting UTXOs containing Ada should not return anything because every
 -- transaction output must have a minimum of 1 ADA. So asking for UTXOs with ADA
@@ -146,6 +139,25 @@ cantRequestForTxOutRefsWithAdaSpec = property $ do
     Left _         -> Hedgehog.assert False
     Right utxoRefs -> Hedgehog.assert $ null utxoRefs
 
+-- | Do not store txs through BlockProcessOption.
+-- The UTxO set must still be stored.
+-- But cannot be fetched through addresses as addresses are not stored.
+doNotStoreTxs :: Property
+doNotStoreTxs = property $ do
+  ((tip, block), state) <- forAll $ Gen.runTxGenState Gen.genNonEmptyBlock
+  result <- liftIO $ Sqlite.withConnection ":memory:" $ \conn -> do
+    Sqlite.runBeamSqlite conn $ autoMigrate Sqlite.migrationBackend checkedSqliteDb
+    liftIO $ runChainIndex conn $ do
+      appendBlock tip block BlockProcessOption{bpoStoreTxs=False}
+      tx <- txFromTxId (view citxTxId (head block))
+      utxosFromAddr <- utxoSetFromBlockAddrs block
+      utxosStored <- traverse utxoSetMembership (S.toList (view Gen.txgsUtxoSet state))
+      pure (tx, concat utxosFromAddr, utxosStored)
+  case result of
+    Right (Nothing, [], utxosStored) -> Hedgehog.assert $ and (snd <$> utxosStored)
+    _                                -> Hedgehog.assert False
+
+-- | Run a chain index action against a SQLite connection.
 runChainIndex
   :: Sqlite.Connection
   -> Eff '[ ChainIndexQueryEffect
