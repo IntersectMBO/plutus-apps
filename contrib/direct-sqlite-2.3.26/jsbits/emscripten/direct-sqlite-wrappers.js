52caafd58c78deb57794a2fd76d8d619c9e4b309
Moritz Angermann <moritz.angermann@gmail.com>
2021-11-17 07:31:07 +0000

    Add a bunch of sqlite functions

--- contrib/direct-sqlite-2.3.26/jsbits/emscripten/direct-sqlite-wrappers.js
+++ contrib/direct-sqlite-2.3.26/jsbits/emscripten/direct-sqlite-wrappers.js
@@ -66,50 +66,125 @@ function h$copyStringToHeap(n, str_d, str_o) {
   return h$getTmpBufferWith(n, str_d, str_o, len);
 }
 
+h$direct_sqlite.h$cstring = function(offset) {
+  var len = 0;
+  while(this.HEAPU8[offset+len] !== 0){ len++; };
+  var str = h$newByteArray(len+1);
+  str.u8.set(this.HEAPU8.subarray(offset,offset+len+1));
+  return str;
+}
+
+h$direct_sqlite.h$ptr = function(offset) {
+  var ptr = h$newByteArray(4);
+  ptr.u8.set(this.HEAPU8.subarray(offset, offset+4));
+  return ptr;
+}
+
 // sqlite3_open ::  CString -> Ptr (Ptr CDatabase) -> IO CError
 function h$sqlite3_open(str_d, str_o, db_d, db_o) {
   var str = h$copyStringToHeap(0, str_d, str_o);
       ptr = h$getTmpBufferWith(1, db_d, db_o, 4);
       ret = h$direct_sqlite._sqlite3_open(str, ptr);
 
-  // allocate new byte array to hold opaque pointer
-  var d = h$newByteArray(4);
-  h$copyFromHeap(ptr, d, 0, 4);
   // make sure to return a pointer! Needs to be a pair! ([d,0])
-  db_d.arr = [[d,0]];
+  db_d.arr = [[h$direct_sqlite.h$ptr(ptr),0]];
   return ret;
 }
 
 // sqlite3_close :: Ptr CDatabase -> IO CError
 function h$sqlite3_close(db_d, db_o) {
-  var ptr = h$getTmpBufferWith(0, db_d, db_o, 4);
+  var ptr = db_d.i3[0];
   return h$direct_sqlite._sqlite3_close(ptr);
 }
 
 // sqlite3_errcode :: Ptr CDatabase -> IO CError
 function h$sqlite3_errcode(db_d, db_o) {
-  var ptr = h$getTmpBufferWith(0, db_d, db_o, 4);
+  var ptr = db_d.i3[0];
   return h$direct_sqlite._sqlite3_close(ptr);
 }
 
-// sqlite3_errmsg :: Ptr CDatabase -> IO CString
 // sqlite3_interrupt :: Ptr CDatabase -> IO ()
+function h$sqlite3_interrupt(db_d, db_o) {
+  var ptr = db_d.i3[0];
+  // we don't really need to return here. it's unit.
+  return h$direct_sqlite._sqlite3_interrupt(ptr);
+}
+
+// sqlite3_errmsg :: Ptr CDatabase -> IO CString
+function h$sqlite3_errmsg(db_d, db_o) {
+  var ptr = db_d.i3[0];
+      charptr = h$direct_sqlite._sqlite3_errmsg(ptr);
+  return h$direct_sqlite.h$cstring(charptr);
+}
 // sqlite3_trace :: Ptr CDatabase -> FunPtr (CTraceCallback a)  -> Ptr a -> IO (Ptr ())
 // sqlite3_get_autocommit :: Ptr CDatabase -> IO CInt
 // sqlite3_enable_shared_cache :: Bool -> IO CError
 // sqlite3_exec :: Ptr CDatabase -> CString -> FunPtr (CExecCallback a) -> Ptr a -> Ptr CString -> IO CError
+function h$sqlite3_exec(db_d, db_o,
+                        sql_d, sql_o,
+                        cb_d, cb_o,
+                        ptr_d, ptr_o,
+                        res_d, res_o) {
+
+  if(cb_d !== null) {
+    console.log("FunPtr don't appear to work in GHCJS.");
+    console.log(cb_d, cb_o);
+    h$exitProcess(1);
+  }
+  var sql = h$copyStringToHeap(0, sql_d, sql_o);
+      cb_ptr = cb_d === null ? null : h$getTmpBufferWith(1, cb_d, cb_o, 8); // FIXME: why 8? FunPtr are 8, and Ptrs only 4?
+      ptr_ptr = ptr_d === null ? null : h$getTmpBufferWith(2, ptr_d, ptr_o, 4);
+      res_ptr = h$getTmpBufferWith(3, res_d, res_o, 4);
+      ret = h$direct_sqlite._sqlite3_exec(db_d.i3[0], sql, null /* cb_ptr */, null, res_ptr);
+  // I don't think we need to read back anything from a FunPtr value. Just pass it.
+  res_d.arr = [[h$direct_sqlite.h$cstring(res_ptr),0]];
+  if(ptr_d !== null) {
+    ptr_d.arr = [[h$direct_sqlite.h$cstring(ptr_ptr),0]]
+  }
+  return ret;
+}
 // sqlite3_prepare_v2 :: Ptr CDatabase -> CString -> CNumBytes -> Ptr (Ptr CStatement) -> Ptr CString -> IO CError
+function h$sqlite3_prepare_v2(db_d, db_o, sql_d, sql_o, bytes, hdl_d, hdl_o, res_d, res_o) {
+  var sql = h$copyStringToHeap(0, sql_d, sql_o);
+      hdl_ptr = h$getTmpBufferWith(1, hdl_d, hdl_o, 4);
+      res_ptr = res_d === null ? null : h$getTmpBufferWith(2, res_d, res_o, 4);
+
+  var ret = h$direct_sqlite._sqlite3_prepare_v2(db_d.i3[0], sql, bytes, hdl_ptr, res_ptr);
+  hdl_d.arr = [[h$direct_sqlite.h$ptr(hdl_ptr),0]];
+  if(res_d !== null) {
+    res_d.arr = [[h$direct_sqlite.h$cstring(res_ptr),0]];
+  }
+  return ret;
+}
 // sqlite3_db_handle :: Ptr CStatement -> IO (Ptr CDatabase)
 // sqlite3_step :: Ptr CStatement -> IO CError
+function h$sqlite3_step(stmt_d, stmt_o) {
+  return h$direct_sqlite._sqlite3_step(stmt_d.i3[0]);
+}
 // sqlite3_step_unsafe :: Ptr CStatement -> IO CError
+function h$sqlite3_step_unsafe(stmt_d, stmt_o) {
+  return h$direct_sqlite._sqlite3_step_unsafe(stmt_d.i3[0]);
+}
 // sqlite3_reset :: Ptr CStatement -> IO CError
+function h$sqlite3_reset(stmt_d, stmt_o) {
+  return h$direct_sqlite._sqlite3_reset(stmt_d.i3[0]);
+}
 // sqlite3_finalize :: Ptr CStatement -> IO CError
+function h$sqlite3_finalize(stmt_d, stmt_o) {
+  return h$direct_sqlite._sqlite3_finalize(stmt_d.i3[0]);
+}
 // sqlite3_clear_bindings :: Ptr CStatement -> IO CError
+function h$sqlite3_clear_bindings(stmt_d, stmt_o) {
+  return h$direct_sqlite._sqlite3_clear_bindings(stmt_d.i3[0]);
+}
 // sqlite3_sql :: Ptr CStatement -> IO CString
 // sqlite3_bind_parameter_count :: Ptr CStatement -> IO CParamIndex
 // sqlite3_bind_parameter_name :: Ptr CStatement -> CParamIndex -> IO CString
 // sqlite3_bind_parameter_index :: Ptr CStatement -> CString -> IO CParamIndex
 // sqlite3_column_count :: Ptr CStatement -> IO CColumnCount
+function h$sqlite3_column_count(stmt_d, stmt_o) {
+  return h$direct_sqlite._sqlite3_column_count(stmt_d.i3[0]);
+}
 // sqlite3_column_name :: Ptr CStatement -> CColumnIndex -> IO CString
 // sqlite3_bind_blob :: Ptr CStatement -> CParamIndex -> Ptr a -> CNumBytes -> Ptr CDestructor -> IO CError
 // sqlite3_bind_zeroblob :: Ptr CStatement -> CParamIndex -> CInt -> IO CError
@@ -118,11 +193,31 @@ function h$sqlite3_errcode(db_d, db_o) {
 // sqlite3_bind_int64    :: Ptr CStatement -> CParamIndex -> Int64 -> IO CError
 // sqlite3_bind_null     :: Ptr CStatement -> CParamIndex -> IO CError
 // sqlite3_column_type   :: Ptr CStatement -> CColumnIndex -> IO CColumnType
+function h$sqlite3_column_type(stmt_d, stmt_o, idx) {
+  return h$direct_sqlite._sqlite3_column_type(stmt_d.i3[0], idx);
+}
 // sqlite3_column_bytes  :: Ptr CStatement -> CColumnIndex -> IO CNumBytes
+function h$sqlite3_column_bytes(stmt_d, stmt_o, idx) {
+  return h$direct_sqlite._sqlite3_column_bytes(stmt_d.i3[0], idx);
+}
 // sqlite3_column_blob   :: Ptr CStatement -> CColumnIndex -> IO (Ptr a)
+function h$sqlite3_column_blob(stmt_d, stmt_o, idx) {
+  var ret = h$direct_sqlite._sqlite3_column_blob(stmt_d.i3[0], idx);
+  return h$direct_sqlite.h$ptr(ret);
+}
 // sqlite3_column_text   :: Ptr CStatement -> CColumnIndex -> IO CString
+function h$sqlite3_column_text(stmt_d, stmt_o, idx) {
+  var ret = h$direct_sqlite._sqlite3_column_text(stmt_d.i3[0], idx);
+  return h$direct_sqlite.h$cstring(ret);
+}
 // sqlite3_column_int64  :: Ptr CStatement -> CColumnIndex -> IO Int64
+function h$sqlite3_column_int64(stmt_d, stmt_o, idx) {
+  return h$direct_sqlite._sqlite3_column_int64(stmt_d.i3[0], idx);
+}
 // sqlite3_column_double :: Ptr CStatement -> CColumnIndex -> IO Double
+function h$sqlite3_column_double(stmt_d, stmt_o, idx) {
+  return h$direct_sqlite._sqlite3_column_double(stmt_d.i3[0], idx);
+}
 // sqlite3_last_insert_rowid :: Ptr CDatabase -> IO Int64
 // sqlite3_changes :: Ptr CDatabase -> IO CInt
 // sqlite3_total_changes :: Ptr CDatabase -> IO CInt
@@ -146,6 +241,10 @@ function h$sqlite3_errcode(db_d, db_o) {
 // sqlite3_result_error    :: Ptr CContext -> CString -> CNumBytes -> IO ()
 // sqlite3_create_collation_v2 :: Ptr CDatabase -> CString -> CInt -> Ptr a -> FunPtr (CCompare a) -> FunPtr (CFuncDestroy a) -> IO CError
 // sqlite3_free :: Ptr a -> IO ()
+function h$sqlite3_free(ptr_d, ptr_o) {
+  console.log(ptr_d, ptr_o);
+  return h$direct_sqlite._sqlite3_free(ptr_d.i3[0]);
+}
 // sqlite3_free_p :: FunPtr (Ptr a -> IO ())
 // sqlite3_enable_load_extension :: Ptr CDatabase -> Bool -> IO CError
 // sqlite3_wal_hook :: Ptr CDatabase -> FunPtr CWalHook -> Ptr a -> IO (Ptr ())
