fb3c09665a94ec63d4d42bbb480c172d357ab901
Moritz Angermann <moritz.angermann@gmail.com>
2021-11-18 06:55:21 +0000

    rename to test_fold?

--- contrib/ghcjs-c-interop/cbits/test.c
+++ contrib/ghcjs-c-interop/cbits/test.c
@@ -1,4 +1,4 @@
-int fold(int (*op)(int, int), int n) {
+int test_fold(int (*op)(int, int), int n) {
     int res = 0;
     for(int i = 0; i <= n; i++) {
         res = op(i,res);
--- contrib/ghcjs-c-interop/src/Test.hs
+++ contrib/ghcjs-c-interop/src/Test.hs
@@ -4,7 +4,7 @@ module Test where
 import Foreign
 
 foreign import ccall "wrapper" createAddPtr :: (Int -> Int -> Int) -> IO (FunPtr (Int -> Int -> Int))
-foreign import ccall "fold" fold :: (FunPtr (Int -> Int -> Int)) -> Int -> Int
+foreign import ccall "test_fold" test_fold :: (FunPtr (Int -> Int -> Int)) -> Int -> Int
 
 add :: Int -> Int -> Int
 add x y = x+y
@@ -14,6 +14,6 @@ doFold = do
     addPtr <- createAddPtr add
     -- you can use addPtr like any other FunPtr (e.g. give it to foreign code)
     print $ foldl (+) 0 [(0::Int)..10]
-    print $ fold addPtr 10
+    print $ test_fold addPtr 10
     -- you MUST free the FunPtr, otherwise it won't be collected
     freeHaskellFunPtr addPtr
