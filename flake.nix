ac5029de2bcf61c0d94d4c7a6846727d1d9d7ae4
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-11-18 23:34:17 +1300

    Use ghcjs with __dirname shim

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       flake = false;
     };
     haskell-nix = {
-      url = "github:input-output-hk/haskell.nix/hkm/ghcjs-dirname-shim";
+      url = "github:input-output-hk/haskell.nix";
       flake = false;
     };
 
