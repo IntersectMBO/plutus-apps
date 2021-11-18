b577b6937ccc28bef5d1abd50d75bd2f608f25c7
Moritz Angermann <moritz.angermann@gmail.com>
2021-11-18 04:39:24 +0000

    Move into lib

---
+++ contrib/ghcjs-c-interop/src/Test.hs
@@ -0,0 +1,19 @@
+{-# language ForeignFunctionInterface #-}
+module Test where
+
+import Foreign
+
+foreign import ccall "wrapper" createAddPtr :: (Int -> Int -> Int) -> IO (FunPtr (Int -> Int -> Int))
+foreign import ccall "fold" fold :: (FunPtr (Int -> Int -> Int)) -> Int -> Int
+
+add :: Int -> Int -> Int
+add x y = x+y
+
+doFold :: IO ()
+doFold = do
+    addPtr <- createAddPtr add
+    -- you can use addPtr like any other FunPtr (e.g. give it to foreign code)
+    print $ foldl (+) 0 [(0::Int)..10]
+    print $ fold addPtr 10
+    -- you MUST free the FunPtr, otherwise it won't be collected
+    freeHaskellFunPtr addPtr
