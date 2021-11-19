9fb2b0ca4e6b166bdae16dbafb682ef29ee3b0a6
Moritz Angermann <moritz.angermann@gmail.com>
2021-11-19 06:57:00 +0000

    bump haskell.nix to bump ghcjs.

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       flake = false;
     };
     haskell-nix = {
-      url = "github:input-output-hk/haskell.nix";
+      url = "github:input-output-hk/haskell.nix/angerman/fix-ghcjs/";
       flake = false;
     };
 
