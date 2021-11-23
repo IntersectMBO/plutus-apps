ee6d1ee35e9eb6f2153874a74a919f3c611adff0
koslambrou <konstantinos.lambrou@iohk.io>
2021-11-23 07:02:29 -0500

    SCP-3064: Fixed failing test case in the Crowdfunding plutus use case (#131)

--- plutus-use-cases/test/Spec/Crowdfunding.hs
+++ plutus-use-cases/test/Spec/Crowdfunding.hs
@@ -33,12 +33,12 @@ import Test.Tasty.Golden (goldenVsString)
 import Test.Tasty.HUnit qualified as HUnit
 import Test.Tasty.QuickCheck hiding ((.&&.))
 
-import Ledger (Value, minAdaTxOut)
+import Ledger (Value)
+import Ledger qualified
 import Ledger.Ada qualified as Ada
 import Ledger.Slot (Slot (..))
 import Ledger.Time (POSIXTime)
 import Ledger.TimeSlot qualified as TimeSlot
-import Ledger.Value (leq)
 import Plutus.Contract hiding (currentSlot, runError)
 import Plutus.Contract.Test
 import Plutus.Contract.Test.ContractModel
@@ -238,10 +238,21 @@ instance ContractModel CrowdfundingModel where
   -- The 'precondition' says when a particular command is allowed.
   precondition s cmd = case cmd of
     CWaitUntil slot -> slot > s ^. currentSlot
-    CContribute w v -> w `notElem` Map.keys (s ^. contractState . contributions)
-                    && w /= (s ^. contractState . ownerWallet)
-                    && s ^. currentSlot < s ^. contractState . endSlot
-                    && Ada.toValue Ledger.minAdaTxOut `leq` v
+    -- In order to contribute, we need to satisfy the constraint where each tx
+    -- output must have at least N Ada.
+    --
+    -- We must make sure that we don't contribute a too high value such that:
+    --   - we can't pay for fees anymore
+    --   - have a tx output of less than N Ada.
+    --
+    -- We suppose the initial balance is 100 Ada. Needs to be changed if
+    -- the emulator initialises the wallets with a different value.
+    CContribute w v -> let currentWalletBalance = Ada.adaOf 100 + Ada.fromValue (s ^. balanceChange w)
+                        in w `notElem` Map.keys (s ^. contractState . contributions)
+                        && w /= (s ^. contractState . ownerWallet)
+                        && s ^. currentSlot < s ^. contractState . endSlot
+                        && Ada.fromValue v >= Ledger.minAdaTxOut
+                        && (currentWalletBalance - Ada.fromValue v) >= (Ledger.minAdaTxOut <> Ledger.maxFee)
     CStart          -> Prelude.not (s ^. contractState . ownerOnline || s ^. contractState . ownerContractDone)
 
   -- To generate a random test case we need to know how to generate a random
