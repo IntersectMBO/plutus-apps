be722f6773a7fe99afa3a765c5cd9b0c48bdc339
Moritz Angermann <moritz.angermann@gmail.com>
2021-12-08 06:03:42 +0000

    Add Setup.hs to foundation

--- contrib/foundation-0.0.26.1/jsbits/bindings.js
+++ contrib/foundation-0.0.26.1/jsbits/bindings.js
@@ -2,3 +2,6 @@
 function h$foundation_sysrandom_linux(buf_d, buf_o, size) {
     return -19; // ENODEV; foundation returns the same for non-linux hosts.
 }
+function foundation_rngV1_generate(newkey_d, newkey_o, dst_d, dst_o, key_d, key_o, bytes) {
+    return 1;
+}
