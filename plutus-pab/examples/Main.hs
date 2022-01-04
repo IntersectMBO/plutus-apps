d3234b299a93da3bcf8a652d5c439ae5094180df
koslambrou <konstantinos.lambrou@iohk.io>
2022-01-04 11:21:23 -0500

    Make plutus-pab's local cluster in it's own library which can be run using user defined builtin contracts. (#205)
    * This commit is part of an more general issue to inverse the dependency between plutus-pab and plutus-use-cases projects.
    
    * Added TODO comments in plutus-pab's file on was needs to be done.
    
    * Changed the name the datatype `ExampleContracts` to singular `ContractExample` which makes more sense.

--- plutus-pab/examples/Main.hs
+++ plutus-pab/examples/Main.hs
@@ -4,10 +4,10 @@ module Main
     ( main
     ) where
 
-import ContractExample (ExampleContracts)
+import ContractExample (ContractExample)
 import Plutus.PAB.Effects.Contract.Builtin qualified as Builtin
 import Plutus.PAB.Run (runWith)
 
 main :: IO ()
 main = do
-    runWith (Builtin.handleBuiltin @ExampleContracts)
+    runWith (Builtin.handleBuiltin @ContractExample)
