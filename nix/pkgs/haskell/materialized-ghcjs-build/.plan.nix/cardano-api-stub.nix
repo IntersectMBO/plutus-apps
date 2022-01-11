a700b3ec7052f6893df051ac5ee1bf7fb4bd8c66
Moritz Angermann <moritz.angermann@gmail.com>
2022-01-11 08:25:55 +0000

    Update Materialized GHCJS

---
+++ nix/pkgs/haskell/materialized-ghcjs-build/.plan.nix/cardano-api-stub.nix
@@ -0,0 +1,72 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "cardano-api-stub"; version = "0.1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "moritz.angermann@gmail.com";
+      author = "Moritz Angermann";
+      homepage = "";
+      url = "";
+      synopsis = "";
+      description = "";
+      buildType = "Simple";
+      isLocal = true;
+      detailLevel = "FullDetails";
+      licenseFiles = [];
+      dataDir = ".";
+      dataFiles = [];
+      extraSrcFiles = [];
+      extraTmpFiles = [];
+      extraDocFiles = [];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."transformers-except" or (errorHandler.buildDepError "transformers-except"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."aeson-pretty" or (errorHandler.buildDepError "aeson-pretty"))
+          (hsPkgs."base16-bytestring" or (errorHandler.buildDepError "base16-bytestring"))
+          (hsPkgs."base58-bytestring" or (errorHandler.buildDepError "base58-bytestring"))
+          (hsPkgs."cborg" or (errorHandler.buildDepError "cborg"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."cardano-binary" or (errorHandler.buildDepError "cardano-binary"))
+          ];
+        buildable = true;
+        modules = [
+          "Cardano/Api/HasTypeProxy"
+          "Cardano/Api/SerialiseCBOR"
+          "Cardano/Api/SerialiseTextEnvelope"
+          "Cardano/Api/Error"
+          "Cardano/Api"
+          ];
+        hsSourceDirs = [ "src" ];
+        };
+      };
+    } // {
+    src = (pkgs.lib).mkDefault (pkgs.fetchgit {
+      url = "15";
+      rev = "minimal";
+      sha256 = "";
+      }) // {
+      url = "15";
+      rev = "minimal";
+      sha256 = "";
+      };
+    postUnpack = "sourceRoot+=/stubs/cardano-api-stub; echo source root reset to \$sourceRoot";
+    }
