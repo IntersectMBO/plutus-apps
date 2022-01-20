7d3b750a41636c216c89e70d3f0e620bfe927283
Jamie Bertram <jamie.bertram@tweag.io>
2022-01-20 15:10:46 -0500

    Add ormolu to nix shell

--- flake.nix
+++ flake.nix
@@ -9,39 +9,42 @@
   };
   outputs = { self, nixpkgs, flake-utils, haskellNix, easy-ps }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
-    let
-      overlays = [ haskellNix.overlay
-        (final: prev: {
-          # This overlay adds our project to pkgs
-          purescript-bridge =
-            final.haskell-nix.project' {
-              src = ./.;
-              compiler-nix-name = "ghc8107";
-            };
-        })
-      ];
-      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-      flake = pkgs.purescript-bridge.flake {};
-    in flake // {
-      # Built by `nix build .`
-      defaultPackage = flake.packages."purescript-bridge:test:purescript-bridge";
-      devShell = pkgs.purescript-bridge.shellFor {
-        withHoogle = true;
-        tools = {
-          cabal = "latest";
-          hlint = "latest";
-          haskell-language-server = "latest";
-        };
-
-        exactDeps = true;
-
-        buildInputs = with pkgs; with import easy-ps { inherit pkgs; }; [
-          ghcid
-          nixpkgs-fmt
-          purs
-          purescript-language-server
-          spago
+      let
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            # This overlay adds our project to pkgs
+            purescript-bridge =
+              final.haskell-nix.project' {
+                src = ./.;
+                compiler-nix-name = "ghc8107";
+              };
+          })
         ];
-      };
-    });
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.purescript-bridge.flake { };
+      in
+      flake // {
+        # Built by `nix build .`
+        defaultPackage = flake.packages."purescript-bridge:test:purescript-bridge";
+        devShell = pkgs.purescript-bridge.shellFor {
+          withHoogle = true;
+          tools = {
+            cabal = "latest";
+            hlint = "latest";
+            haskell-language-server = "latest";
+          };
+
+          exactDeps = true;
+
+          buildInputs = with pkgs; with import easy-ps { inherit pkgs; }; [
+            ghcid
+            nixpkgs-fmt
+            purs
+            purescript-language-server
+            spago
+            haskellPackages.ormolu
+          ];
+        };
+      });
 }
