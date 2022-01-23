34d0cf8a8e9ed580b55a1ff4a852012ff68d609d
Radu Ometita <radu.ometita@gmail.com>
2022-01-23 20:19:26 +0200

    Added a sanity check for cases where we pass in depth <= 0

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,16 +3,28 @@ import qualified Test.Tasty.QuickCheck as QC
 
 import           Data.Maybe            (isNothing)
 
-import           Model
+import           Test.QuickCheck       (Blind (Blind), Fun (Fun), pattern Fn2,
+                                        NonPositive (NonPositive))
+
+import           Model                 (new)
 
 tests :: TestTree
 tests = testGroup "Utxo index" [hfProperties]
 
 hfProperties :: TestTree
-hfProperties = testGroup "Historical fold" []
+hfProperties = testGroup "Historical fold"
+  [ QC.testProperty "Negative or zero depth" $ prop_hfNewReturnsNothing @Int @Int
+  ]
 
-prop_hfNewReturnsNothing :: (a -> b -> a) -> Int -> a -> Bool
-prop_hfNewReturnsNothing fn depth acc = isNothing $ new fn depth acc
+prop_hfNewReturnsNothing
+  :: Fun (a, b) a
+  -> NonPositive Int
+  -> a
+  -> Bool
+prop_hfNewReturnsNothing
+  (Fn2 fn)
+  (NonPositive depth)
+  acc = isNothing $ new fn depth acc
 
 main :: IO ()
 main = defaultMain tests
