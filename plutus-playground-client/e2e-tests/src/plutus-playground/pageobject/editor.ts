daab3265033010927ce4816ce5b197b08b80dfa3
James <74595920+james-iohk@users.noreply.github.com>
2022-02-22 12:41:29 +0000

    Playground E2E tests (#279)
    * Introduce playwright end-to-end tests
    
    * 1st test
    
    * Polish 1st test and add more to readme
    
    * 2 sanity tests, CI configured, and html report
    
    * move e2e-tests into plutus-playground-client

---
+++ plutus-playground-client/e2e-tests/src/plutus-playground/pageobject/editor.ts
@@ -0,0 +1,63 @@
+import { test, expect } from '@playwright/test';
+
+export class EditorPage {
+  readonly wonderfulText :string = 'wonderful';
+  readonly notCompiledText = 'Not compiled';
+  readonly compilingText = 'Compiling ...';
+  readonly compilationSuccessfulText = 'Compilation successful';
+
+  constructor(page) {
+    this.page = page;
+    this.title = this.page.locator("text=Plutus playground");
+    this.h1 = this.page.locator("h1 >> text=Editor");
+    this.helloWorld = this.page.locator("a >> text=Hello, world");
+    this.starter = this.page.locator("a >> text=Starter");
+    this.game = this.page.locator("a >> text=Game");
+    this.vesting = this.page.locator("a >> text=Vesting");
+    this.crowdFunding = this.page.locator("a >> text=Crowd Funding");
+    this.errorHandling = this.page.locator("a >> text=Error Handling");
+    this.helloWorldInEditor = this.page.locator("//div[@class='code-editor']/descendant::span[contains(text(), 'Hello') and contains(text(), 'world')]"); // text in code editor
+    this.compilingInEditor = this.page.locator("//div[@class='code-editor']/descendant::span[contains(text(), 'Crowdfunding')]"); // text in code editor
+    this.compileBtn = this.page.locator("button >> text=Compile");
+    this.simulateBtn = this.page.locator("//button >> text=Simulate");
+    this.feedbackHeader = this.page.locator("//div[@class='editor-feedback-header']");
+  }
+
+  async navigate(base: string) {
+    await this.page.goto(base);
+    await expect(this.title).toHaveText('Plutus playground');
+    await expect(this.h1).toHaveText('Editor');
+  }
+
+  async modifyHelloWorldText(text: string) {
+    await expect(this.feedbackHeader).toHaveText(this.compilationSuccessfulText); // it's already compiled becuse it's an example?
+    await this.helloWorldInEditor.click();
+    await this.page.keyboard.type(text);
+  }
+
+  async compileHelloWorld(): Promise<string> {
+    const textToInsert = this.wonderfulText;
+    await this.helloWorld.click();
+    await this.modifyHelloWorldText(textToInsert);
+    await expect(this.helloWorldInEditor).toBeVisible();
+    await this.compile();
+    return textToInsert;
+  }
+
+  async compileCrowdFunding() {
+    await this.crowdFunding.click();
+    await expect(this.compilingInEditor).toBeVisible();
+    await this.compile();
+  }
+
+  async compile() {
+    await this.compileBtn.click();
+    await expect(this.feedbackHeader).toHaveText(this.compilingText);
+    await expect(this.feedbackHeader).toHaveText(this.compilationSuccessfulText);
+  }
+
+  async simulate() {
+    await this.simulateBtn.click();
+  }
+
+}
