38ffe2a8cf6488f516f7a0e93e7fc2c928deccbf
Evgenii Akentev <i@ak3n.com>
2022-03-24 11:04:23 +0400

    remove isFull bool flag

--- plutus-chain-index/src/Control/Concurrent/STM/TBMQueue.hs
+++ plutus-chain-index/src/Control/Concurrent/STM/TBMQueue.hs
@@ -9,9 +9,8 @@ module Control.Concurrent.STM.TBMQueue
   ) where
 
 import Control.Concurrent.STM (STM, retry)
-import Control.Concurrent.STM.TQueue (TQueue, flushTQueue, isEmptyTQueue, newTQueue, newTQueueIO, writeTQueue)
+import Control.Concurrent.STM.TQueue (TQueue, flushTQueue, newTQueue, newTQueueIO, writeTQueue)
 import Control.Concurrent.STM.TVar (TVar, newTVar, newTVarIO, readTVar, writeTVar)
-import Control.Monad (when)
 import Numeric.Natural (Natural)
 
 
@@ -19,7 +18,6 @@ import Numeric.Natural (Natural)
 -- 'TBMQueue' with a measure function 'const 1' should be equivalent to 'TBQueue'.
 data TBMQueue a = TBMQueue !(TQueue a)
                            {-# UNPACK #-} !(TVar Natural) -- Current size
-                           {-# UNPACK #-} !(TVar Bool) -- Is the queue full? Is the last item too big?
                            !Natural -- Max size
                            !(a -> Natural) -- Measure function
 
@@ -27,51 +25,39 @@ data TBMQueue a = TBMQueue !(TQueue a)
 newTBMQueue :: Natural -> (a -> Natural) -> STM (TBMQueue a)
 newTBMQueue maxSize measure = do
   queue <- newTQueue
-  isFull <- newTVar False
   currentSize <- newTVar 0
-  return $ TBMQueue queue currentSize isFull maxSize measure
+  return $ TBMQueue queue currentSize maxSize measure
 
 -- | 'IO' version of 'newTBMQueue'.
 newTBMQueueIO :: Natural -> (a -> Natural) -> IO (TBMQueue a)
 newTBMQueueIO maxSize measure = do
   queue <- newTQueueIO
-  isFull <- newTVarIO False
   currentSize <- newTVarIO 0
-  return $ TBMQueue queue currentSize isFull maxSize measure
+  return $ TBMQueue queue currentSize maxSize measure
 
 -- | Write a value to a 'TBMQueue'; blocks if the queue is full.
 writeTBMQueue :: TBMQueue a -> a -> STM ()
-writeTBMQueue (TBMQueue q currentSize isFull maxSize measure) item = do
-  full <- readTVar isFull
-  if full then retry
+writeTBMQueue (TBMQueue q currentSize maxSize measure) item = do
+  size <- readTVar currentSize
+  if size > maxSize then retry
   else do
-    current <- readTVar currentSize
-    let newCurrentSize = measure item + current
-    if newCurrentSize <= maxSize then do
-      writeTQueue q item
-      writeTVar currentSize newCurrentSize
-    else do
-      isQueueEmpty <- isEmptyTQueue q
-      -- if queue is empty and an item is too big to insert
-      -- we still insert it alone to avoid an infinite loop
-      when isQueueEmpty $ do
-        writeTQueue q item
-        writeTVar currentSize newCurrentSize
-      writeTVar isFull True
+    writeTQueue q item
+    writeTVar currentSize (measure item + size)
 
 -- | Returns 'True' if the supplied 'TBMQueue' is full.
 isFullTBMQueue :: TBMQueue a -> STM Bool
-isFullTBMQueue (TBMQueue _ _ isFull _ _) = readTVar isFull
+isFullTBMQueue (TBMQueue _ currentSize maxSize _) = do
+  size <- readTVar currentSize
+  return $ size > maxSize
 
 -- | Efficiently read the entire contents of a 'TBMQueue' into a list. This
 -- function never retries.
 flushTBMQueue :: TBMQueue a -> STM [a]
-flushTBMQueue (TBMQueue q currentSize isFull _ _) = do
+flushTBMQueue (TBMQueue q currentSize _ _) = do
   items <- flushTQueue q
   writeTVar currentSize 0
-  writeTVar isFull False
   return items
 
 -- | Returns current size of the queue.
 sizeTBMQueue :: TBMQueue a -> STM Natural
-sizeTBMQueue (TBMQueue _ currentSize _ _ _) = readTVar currentSize
+sizeTBMQueue (TBMQueue _ currentSize _ _) = readTVar currentSize
