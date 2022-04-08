a2450bf92b39a15f20f18255f28044831abec823
etiennjf <jean-frederic.etienne@iohk.io>
2022-04-08 14:05:54 +0200

    make contractState mutable to avoid bottleneck when updating InMemInstances

--- plutus-pab/src/Plutus/PAB/Db/Memory/ContractStore.hs
+++ plutus-pab/src/Plutus/PAB/Db/Memory/ContractStore.hs
@@ -19,12 +19,13 @@ module Plutus.PAB.Db.Memory.ContractStore(
 
 import Control.Concurrent.STM (TVar)
 import Control.Concurrent.STM qualified as STM
-import Control.Lens (_Just, at, makeLensesFor, preview, set)
+import Control.Lens (at, set)
 import Control.Monad.Freer (Eff, LastMember, Member, type (~>))
 import Control.Monad.Freer.Error (Error, throwError)
 import Control.Monad.Freer.Reader (Reader, ask)
 import Control.Monad.IO.Class (MonadIO (..))
 import Data.Map (Map)
+import Data.Map qualified as Map
 import Plutus.PAB.Effects.Contract (ContractStore)
 import Plutus.PAB.Effects.Contract qualified as Contract
 import Plutus.PAB.Types (PABError (..))
@@ -32,13 +33,14 @@ import Plutus.PAB.Webserver.Types (ContractActivationArgs)
 import Wallet.Types (ContractInstanceId)
 
 -- | The current state of a contract instance
+-- Considering InstanceState contractState as mutable to avoid bottleneck when updating InMemInstances especially
+-- when an instance already exists. Note also that PutState is adjusted so as to avoid full map update
+-- when instance already exists.
 data InMemContractInstanceState t =
     InMemContractInstanceState
         { _contractDef   :: ContractActivationArgs (Contract.ContractDef t)
-        , _contractState :: Contract.State t
+        , _contractState :: TVar (Contract.State t)
         }
-
-makeLensesFor [("_contractState", "contractState")] ''InMemContractInstanceState
 
 newtype InMemInstances t = InMemInstances { unInMemInstances :: TVar (Map ContractInstanceId (InMemContractInstanceState t)) }
 
@@ -59,14 +61,25 @@ handleContractStore = \case
     Contract.PutState definition instanceId state -> do
         instancesTVar <- unInMemInstances <$> ask @(InMemInstances t)
         liftIO $ STM.atomically $ do
-            let instState = InMemContractInstanceState{_contractDef = definition, _contractState = state}
-            STM.modifyTVar instancesTVar (set (at instanceId) (Just instState))
+          instances <- STM.readTVar instancesTVar
+          case Map.lookup instanceId instances of
+              Nothing -> do
+                -- adding new entry
+                stateTVar <- STM.newTVar state
+                let instState = InMemContractInstanceState{_contractDef = definition, _contractState = stateTVar}
+                STM.modifyTVar instancesTVar (set (at instanceId) (Just instState))
+              Just oldInstState -> do
+                -- only update state
+                STM.writeTVar (_contractState oldInstState) state
+
     Contract.GetState instanceId -> do
         instancesTVar <- unInMemInstances <$> ask @(InMemInstances t)
-        result <- preview (at instanceId . _Just . contractState) <$> liftIO (STM.readTVarIO instancesTVar)
-        case result of
-            Just s  -> pure s
-            Nothing -> throwError (ContractInstanceNotFound instanceId)
+        instances <- liftIO $ STM.readTVarIO instancesTVar
+        case Map.lookup instanceId instances of
+          Nothing -> throwError (ContractInstanceNotFound instanceId)
+          Just instState ->
+            liftIO $ STM.atomically $ STM.readTVar $ _contractState instState
+
     Contract.GetContracts _ -> do
         instancesTVar <- unInMemInstances <$> ask @(InMemInstances t)
         fmap _contractDef <$> liftIO (STM.readTVarIO instancesTVar)
