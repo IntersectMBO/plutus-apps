9e355af1fa42cba0b9bd8dd1f1a7e73b09483a21
Radu Ometita <radu.ometita@gmail.com>
2022-04-12 00:43:49 +0700

    Make the sqlindex pass tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,9 +3,10 @@ import           Test.Tasty.QuickCheck
 
 import qualified Spec.Index            as Ix
 import qualified Spec.Split            as S
+import qualified Spec.Sqlite           as Sqlite
 
 tests :: TestTree
-tests = testGroup "Index" [ixProperties, siProperties]
+tests = testGroup "Index" [ixProperties, siProperties, sqProperties]
 
 ixProperties :: TestTree
 ixProperties = testGroup "Basic model"
@@ -41,6 +42,24 @@ siProperties = testGroup "Split index"
       withMaxSuccess 10000 $ Ix.prop_observeNotifications @Int @Int @Int S.conversion
   ,  testProperty "Notifications are not affected by rewind" $
       withMaxSuccess 1000 $ Ix.prop_insertRewindNotifications @Int @Int @Int S.conversion
+  ]
+
+sqProperties :: TestTree
+sqProperties = testGroup "Sqlite index"
+  [ testProperty "New: Positive or non-positive depth" $
+      withMaxSuccess 10000 $ Ix.prop_observeNew @Int @Int @Int Sqlite.conversion
+  , testProperty "History length is always smaller than the max depth" $
+      withMaxSuccess 10000 $ Ix.prop_sizeLEDepth @Int @Int @Int Sqlite.conversion
+  , testProperty "Rewind: Connection with `ixDepth`" $
+      withMaxSuccess 10000 $ Ix.prop_rewindDepth @Int @Int @Int Sqlite.conversion
+  , testProperty "Relationship between Insert/Rewind" $
+      withMaxSuccess 10000 $ Ix.prop_insertRewindInverse @Int @Int @Int Sqlite.conversion
+  , testProperty "Insert is folding the structure" $
+      withMaxSuccess 10000 $ Ix.prop_observeInsert @Int @Int @Int Sqlite.conversion
+  , testProperty "Notifications are accumulated as the fold runs" $
+      withMaxSuccess 10000 $ Ix.prop_observeNotifications @Int @Int @Int Sqlite.conversion
+  ,  testProperty "Notifications are not affected by rewind" $
+      withMaxSuccess 1000 $ Ix.prop_insertRewindNotifications @Int @Int @Int Sqlite.conversion
   ]
 
 main :: IO ()
--- test/Spec/Sqlite.hs
+++ test/Spec/Sqlite.hs
@@ -2,7 +2,9 @@ module Spec.Sqlite where
 
 import           Control.Monad.IO.Class  (liftIO)
 import           Data.Maybe              (catMaybes)
-import           Database.SQLite.Simple  (execute_, execute, Only(..))
+import           Database.SQLite.Simple  (Connection, query, execute_, execute, Only(..))
+import Database.SQLite.Simple.ToField
+import Database.SQLite.Simple.FromField
 import           Test.QuickCheck         (Property)
 import           Test.QuickCheck.Monadic (PropertyM, monadicIO)
 import qualified Test.QuickCheck.Monadic as M
@@ -10,28 +12,20 @@ import qualified Test.QuickCheck.Monadic as M
 import           Index                   (Index, IndexView (..))
 import qualified Index                   as Ix
 import           Index.Split             (SplitIndex(..))
-import           Index.Sqlite            (SqliteIndex (..))
+import           Index.Sqlite            (SqliteIndex)
 import qualified Index.Sqlite            as S
 import           Spec.Index              (Conversion (..))
 
-conversion :: (Show e, Show n, Show a) => Conversion (PropertyM IO) a e n
+conversion :: (Show e, Show n, Show a, ToField a, FromField a) => Conversion (PropertyM IO) a e n
 conversion = Conversion
   { cView          = view
-  , cHistory       = undefined
-  , cNotifications = undefined
-  , cMonadic       = undefined
+  , cHistory       = history
+  , cNotifications = notifications
+  , cMonadic       = monadic
   }
 
--- conversion :: (Show a, Show e, Show n) => Conversion (PropertyM IO) a e n
--- conversion = Conversion
---   { cView          = view
---   , cHistory       = history
---   , cNotifications = notifications
---   , cMonadic       = monadic
---   }
-
 view
-  :: (Show a, Show e, Show n)
+  :: (Show a, ToField a, FromField a, Show e, Show n)
   => Index a e n
   -> PropertyM IO (Maybe (IndexView a))
 view ix = do
@@ -42,76 +36,71 @@ view ix = do
       v <- M.run $ S.view ix'
       pure $ Just v
 
--- notifications
---   :: (Show a, Show e, Show n)
---   => Index a e n
---   -> PropertyM IO [n]
--- notifications ix = do
---   -- We should never call this on invalid indexes.
---   Just ix' <- run ix
---   S.getNotifications ix'
+notifications
+  :: (Show a, ToField a, FromField a, Show e, Show n)
+  => Index a e n
+  -> PropertyM IO [n]
+notifications ix = do
+  -- We should never call this on invalid indexes.
+  Just ix' <- run ix
+  liftIO $ S.getNotifications ix'
 
--- history
---   :: (Show a, Show e, Show n)
---   => Index a e n
---   -> PropertyM IO (Maybe [a])
--- history ix = do
---   mix <- run ix
---   case mix of
---     Nothing  -> pure Nothing
---     Just ix' -> do
---       h <- S.getHistory ix'
---       pure $ Just h
+history
+  :: (Show a, ToField a, FromField a, Show e, Show n)
+  => Index a e n
+  -> PropertyM IO (Maybe [a])
+history ix = do
+  mix <- run ix
+  case mix of
+    Nothing  -> pure Nothing
+    Just ix' -> liftIO $ do
+      h <- S.getHistory ix'
+      pure $ Just h
 
--- monadic
---   :: PropertyM IO Property
---   -> Property
--- monadic = monadicIO
+monadic
+  :: PropertyM IO Property
+  -> Property
+monadic = monadicIO
 
 run
-  :: forall a e n. (Show a, Show e, Show n)
+  :: forall a e n. (Show a, ToField a, FromField a, Show e, Show n)
   => Index a e n
   -> PropertyM IO (Maybe (SqliteIndex a e n))
-run (Ix.New f d a) = undefined
--- run (Ix.New f d a) = do
---   Just sqliteIndex <- liftIO $ S.new findex fstore d "sqlite-index-property-tests.sqlite"
---   let (PartialStore c _) = siStoredIx sqliteIndex
---   -- Initialise database
---   liftIO $ do
---     execute_ c "DROP TABLE IF EXISTS sqlite-index-property-tests"
---     execute_ c "CREATE TABLE sqlite-index-property-tests (id INTEGER PRIMARY KEY, accumulator TEXT)"
---     execute  c "INSERT INTO sqlite-index-property-tests (id, accumulator) VALUES ?" (1, a)
---   pure $ Just sqliteIndex
---   where
---     findex :: PartialStore e -> [e] -> IO (PartialStore e, [n])
---     -- TODO: Is es' always supposed to be empty when calling this function?
---     findex (PartialStore c es') es = do
---       -- TODO: Potential race condition (do we care?)
---       storedAcc <- execute c "SELECT (accumulator) from sqlite-index-property-tests WHERE id = ?" (Only 1)
---       pure $ foldr convertIxF (storedAcc, []) (es' ++ es)
---     fstore :: PartialStore e -> IO (PartialStore e)
---     fstore (PartialStore c es) = do
---       execute_ c "BEGIN"
---       storedAcc <- execute c "SELECT (accumulator) FROM sqlite-index-property-tests WHERE id = ?" (Only 1)
---       let nextAcc = fst $ foldr convertIxF (storedAcc, []) es
---       execute c "UPDATE sqlite-index-property-tests (accumulator = ?) WHERE id = ?" (nextAcc, 1)
---       execute c "COMMIT"
---       pure $ PartialStore c []
---     convertIxF :: e -> (PartialStore e, [n]) -> (PartialStore e, [n])
---     convertIxF e (a', ns) =
---       let (a'', mn) = f a' e
---        in (a'', catMaybes [mn] ++ ns)
-run (Ix.Insert e ix) = undefined
--- run (Ix.Insert e ix) = do
---   mix <- run ix
---   case  mix of
---     Nothing  -> pure Nothing
---     Just ix' -> do
---       nix <- S.insert e ix'
---       pure $ Just nix
-run (Ix.Rewind n ix) = undefined
--- run (Ix.Rewind n ix) = do
---   mix <- run ix
---   case mix of
---     Nothing  -> pure Nothing
---     Just ix' -> pure $ S.rewind n ix'
+run (Ix.New f depth acc) = do
+  sqliteIndex <- liftIO $ S.new findex fstore fload depth ":memory:"
+  case sqliteIndex of
+    Nothing -> pure Nothing
+    Just ix -> do
+      let c = siHandle ix
+      -- Initialise database
+      liftIO $ do
+        execute_ c "DROP TABLE IF EXISTS index_property_tests"
+        execute_ c "CREATE TABLE index_property_tests (id INTEGER PRIMARY KEY, accumulator INT)"
+        execute  c "INSERT INTO index_property_tests (id, accumulator) VALUES (?, ?)" (1 :: Int, acc)
+      pure . Just $ ix
+  where
+    findex :: a -> [e] -> (a, [n])
+    findex a es = foldr convertIxF (a, []) es
+    fstore :: Connection -> a -> IO ()
+    fstore c a =
+      execute c "UPDATE index_property_tests SET accumulator = ? WHERE id = ?" (a, 1 :: Int)
+    fload  :: Connection -> IO a
+    fload c = do
+      [[a]] <- query c "SELECT (accumulator) FROM index_property_tests WHERE id = ?" (Only 1 :: Only Int)
+      pure a
+    convertIxF :: e -> (a, [n]) -> (a, [n])
+    convertIxF e (a, ns) =
+      let (a', mn) = f a e
+       in (a', catMaybes [mn] ++ ns)
+run (Ix.Insert e ix) = do
+  mix <- run ix
+  case  mix of
+    Nothing  -> pure Nothing
+    Just ix' -> liftIO $ do
+      nix <- S.insert e ix'
+      pure $ Just nix
+run (Ix.Rewind n ix) = do
+  mix <- run ix
+  case mix of
+    Nothing  -> pure Nothing
+    Just ix' -> liftIO . pure $ S.rewind n ix'
