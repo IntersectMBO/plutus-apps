3eaf1733a9a619314c2ff7468f94ba0e8c5d8997
Tomasz Rybarczyk <tomasz.rybarczyk@iohk.io>
2022-05-20 12:01:47 +0200

    $ updateMaterialized

--- nix/pkgs/haskell/materialized-linux/.plan.nix/plutus-contract.nix
+++ nix/pkgs/haskell/materialized-linux/.plan.nix/plutus-contract.nix
@@ -109,7 +109,6 @@
           "Plutus/Contract/StateMachine/ThreadToken"
           "Plutus/Contract/Tx"
           "Plutus/Contract/Types"
-          "Plutus/Contract/Unsafe"
           "Plutus/Contract/Util"
           "Plutus/Contract/Wallet"
           "Plutus/Contract/Typed/Tx"
