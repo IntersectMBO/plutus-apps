53929b596eb017cee1b1b6e485a42cf69d62ff48
Konstantinos Lambrou-Latreille <konstantinos.lambrou@iohk.io>
2022-05-25 15:34:46 -0400

    PLT-161 Updated cardano-node et al. dependencies to the latest version

--- nix/pkgs/haskell/sha256map.nix
+++ nix/pkgs/haskell/sha256map.nix
@@ -1,21 +1,23 @@
 {
   "https://github.com/input-output-hk/ekg-forward"."297cd9db5074339a2fb2e5ae7d0780debb670c63" = "1zcwry3y5rmd9lgxy89wsb3k4kpffqji35dc7ghzbz603y1gy24g";
   "https://github.com/input-output-hk/cardano-addresses"."56746f558d57a6b1d66d20eaac8a83dc4455cb4f" = "sha256-zo+XJJsIiSkUlcxuPhRQRCxcWYOkDXnAApBF9Pj5Tls=";
-  "https://github.com/input-output-hk/cardano-base"."394c4637c24d82325bd04ceb99c8e8df5617e663" = "02q8y69za5b0vsnj9qga1364vkmfc1kh35d0yshw1lf7nw9bls8m";
+  "https://github.com/input-output-hk/cardano-base"."631cb6cf1fa01ab346233b610a38b3b4cba6e6ab" = "0944wg2nqazmhlmsynwgdwxxj6ay0hb9qig9l128isb2cjia0hlp";
   "https://github.com/input-output-hk/cardano-config"."e9de7a2cf70796f6ff26eac9f9540184ded0e4e6" = "1wm1c99r5zvz22pdl8nhkp13falvqmj8dgkm8fxskwa9ydqz01ld";
   "https://github.com/input-output-hk/cardano-crypto"."f73079303f663e028288f9f4a9e08bcca39a923e" = "1n87i15x54s0cjkh3nsxs4r1x016cdw1fypwmr68936n3xxsjn6q";
-  "https://github.com/input-output-hk/cardano-ledger"."1db68a3ec0a2dcb5751004beb22b906162474f23" = "03pv2jvskbi65dwaddp6a8bxbbcw674csjxhg8xbqd6q1kfpc41a";
-  "https://github.com/input-output-hk/cardano-node"."65422ff373f2f88a563afa746a9a16d211ffdc7c" = "0rvwf2nmfn8yad0004qfp1racygriqrd7fd0z2wgbjgh18dfnin2";
+  "https://github.com/input-output-hk/cardano-ledger"."c415253fff9a5ce9d7575230fc9098bcfee97653" = "1m4hq8yxcm7ny5vjzqkrl9qn9zmpr8c9xs0c98ix9hm9xd79fapc";
+  "https://github.com/input-output-hk/cardano-node"."67f30e25f23a7bd5e500e0da2366f8a2ca4b6932" = "0qfix57gjxmgjkjl573ffpmf4yk1zz3n6sn79wbw7q1gzvn8h0yn";
   "https://github.com/input-output-hk/cardano-prelude"."bb4ed71ba8e587f672d06edf9d2e376f4b055555" = "sha256-kgX3DKyfjBb8/XcDEd+/adlETsFlp5sCSurHWgsFAQI=";
   "https://github.com/ak3n/cw"."538375cb3efa0c66604ff0b13b6ecc0c4a694d64" = "sha256-rCVX42d2nM67aWBPUu2wdMAGQ6s25VJcrEZwUUYa800=";
   "https://github.com/input-output-hk/goblins"."cde90a2b27f79187ca8310b6549331e59595e7ba" = "sha256-z9ut0y6umDIjJIRjz9KSvKgotuw06/S8QDwOtVdGiJ0=";
   "https://github.com/input-output-hk/hedgehog-extras"."967d79533c21e33387d0227a5f6cc185203fe658" = "0rbqb7a64aya1qizlr3im06hdydg9zr6sl3i8bvqqlf7kpa647sd";
   "https://github.com/input-output-hk/iohk-monitoring-framework"."eb7854d1337637b8672af1227b276aa33a658f47" = "1ll81hlhkhj96f5v6lswjkq2h8f7zcmdrj2azqhi4ylzafn026r3";
+  "https://github.com/input-output-hk/io-sim"."606de33fa2f467d108fb1efb86daeb3348bf34e3" = "048djqfdkzrbmdz670knbkjy12wddpvsmhm4x5yldg3w9lwqi1yx";
   "https://github.com/input-output-hk/optparse-applicative"."7497a29cb998721a9068d5725d49461f2bba0e7a" = "1gvsrg925vynwgqwplgjmp53vj953qyh3wbdf34pw21c8r47w35r";
-  "https://github.com/input-output-hk/ouroboros-network"."ad47441e9e399495579d7dd05ccaf96b46a487b2" = "1a54yfc4m9n5j699cn8ln524h28043dyrlzn34wmnhsgqnxhavr4";
-  "https://github.com/input-output-hk/plutus"."5cc518f1202930ad52b8ba838af32af084c0e754" = "sha256-bmkP98dcMdDDmAk5UsceYh0Wme19Z0squHWJnY8FO3E=";
+  "https://github.com/input-output-hk/ouroboros-network"."ea202b7b21983140d1944ccfde8750b891b59699" = "0bixx0b61sjzls7hzwjlgmvl0yk1ckd40cfazda8b3smqvxfrphq";
+  "https://github.com/input-output-hk/plutus"."fec94223a985e34d3b270460c8f150002f41b85b" = "1mwknikvbpv9lj43c3ya24v7wpcbpcsrk0fnh6knpi7ds4rmj9j0";
   "https://github.com/input-output-hk/purescript-bridge"."47a1f11825a0f9445e0f98792f79172efef66c00" = "0da1vn2l6iyfxcjk58qal1l4755v92zi6yppmjmqvxf1gacyf9px";
   "https://github.com/input-output-hk/servant-purescript"."44e7cacf109f84984cd99cd3faf185d161826963" = "10pb0yfp80jhb9ryn65a4rha2lxzsn2vlhcc6xphrrkf4x5lhzqc";
+  "https://github.com/input-output-hk/typed-protocols"."91c3fba44d68439df207796171cd6f867354b76b" = "1h1nh10859kd7j8bb55y577jfdp42aqpppx8fn0pm6fra5748a50";
   "https://github.com/input-output-hk/Win32-network"."3825d3abf75f83f406c1f7161883c438dac7277d" = "19wahfv726fa3mqajpqdqhnl9ica3xmf68i254q45iyjcpj1psqx";
   "https://github.com/Quid2/flat"."ee59880f47ab835dbd73bea0847dab7869fc20d8" = "1lrzknw765pz2j97nvv9ip3l1mcpf2zr4n56hwlz0rk7wq7ls4cm";
   "https://github.com/vshabanov/ekg-json"."00ebe7211c981686e65730b7144fbf5350462608" = "sha256-VT8Ur585TCn03P2TVi6t92v2Z6tl8vKijICjse6ocv8=";
