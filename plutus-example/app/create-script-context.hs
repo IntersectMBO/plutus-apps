72e756286c6ae543417d315c068a3842ddaa656a
Jordan Millar <jordan.millar@iohk.io>
2022-05-27 15:08:42 -0500

    Propagate the changes to the plutus-example apps and tests

--- plutus-example/app/create-script-context.hs
+++ plutus-example/app/create-script-context.hs
@@ -10,6 +10,7 @@ import Data.Word
 import Options.Applicative
 import Options.Applicative qualified as Opt
 
+import Plutus.ApiCommon
 import PlutusExample.ScriptContextChecker
 
 main :: IO ()
@@ -53,8 +54,11 @@ parseScriptContextCmd = parseGenerateDummy <|> parseGenerateTxBody
 
 data ScriptContextCmd
   = GenerateDummyScriptContextRedeemer
+      --LedgerPlutusVersion TODO: Babbage era. We should
+      -- parameterize on LedgerPlutusVersion.
       FilePath
   | GenerateScriptContextRedeemerTxBody
+     -- LedgerPlutusVersion
       FilePath
       AnyConsensusModeParams
       NetworkId
@@ -62,9 +66,9 @@ data ScriptContextCmd
 
 runScriptContextCmd :: ScriptContextCmd -> IO ()
 runScriptContextCmd (GenerateDummyScriptContextRedeemer outFp) =
-  LB.writeFile outFp sampleTestScriptContextDataJSON
+  LB.writeFile outFp sampleTestV1ScriptContextDataJSON
 runScriptContextCmd (GenerateScriptContextRedeemerTxBody txbodyfile cModeParams nid outFp) = do
-      eTxBodyRedeemer <- runExceptT $ txToRedeemer txbodyfile cModeParams nid
+      eTxBodyRedeemer <- runExceptT $ createAnyCustomRedeemerBsFromTxFp txbodyfile cModeParams nid
       case eTxBodyRedeemer of
         Left err -> error $ "Error creating redeemer from: " <> txbodyfile <>
                             " Error: " <> show err
--- plutus-example/app/plutus-example.hs
+++ plutus-example/app/plutus-example.hs
@@ -6,28 +6,36 @@ import Cardano.Api
 import System.Directory
 import System.FilePath.Posix ((</>))
 
-import PlutusExample.AlwaysFails (alwaysFailsScript)
-import PlutusExample.AlwaysSucceeds (alwaysSucceedsScript)
-import PlutusExample.CustomDatumRedeemerGuess
-import PlutusExample.DatumRedeemerGuess (guessScript, guessScriptStake)
-import PlutusExample.Loop (loopScript)
-import PlutusExample.MintingScript (apiExamplePlutusMintingScript)
-import PlutusExample.ScriptContextChecker
-import PlutusExample.Sum (sumScript)
+import PlutusExample.PlutusVersion1.AlwaysFails (alwaysFailsScript)
+import PlutusExample.PlutusVersion1.AlwaysSucceeds (alwaysSucceedsScript)
+import PlutusExample.PlutusVersion1.CustomDatumRedeemerGuess
+import PlutusExample.PlutusVersion1.DatumRedeemerGuess (guessScript, guessScriptStake)
+import PlutusExample.PlutusVersion1.Loop (loopScript)
+import PlutusExample.PlutusVersion1.MintingScript (apiExamplePlutusMintingScript)
+import PlutusExample.PlutusVersion1.RedeemerContextScripts
+import PlutusExample.PlutusVersion1.Sum (sumScript)
+
+import PlutusExample.PlutusVersion2.RequireRedeemer (requireRedeemerScript)
 
 main :: IO ()
 main = do
-  let dir = "generated-plutus-scripts"
-  createDirectory dir
+  let v1dir = "generated-plutus-scripts/v1"
+      v2dir = "generated-plutus-scripts/v2"
+  createDirectoryIfMissing True v1dir
+  createDirectoryIfMissing True v2dir
 
-  _ <- writeFileTextEnvelope (dir </> "always-fails.plutus") Nothing alwaysFailsScript
-  _ <- writeFileTextEnvelope (dir </> "always-succeeds-spending.plutus") Nothing alwaysSucceedsScript
-  _ <- writeFileTextEnvelope (dir </> "guess-42-datum-42-txin.plutus") Nothing guessScript
-  _ <- writeFileTextEnvelope (dir </> "guess-42-stake.plutus") Nothing guessScriptStake
-  _ <- writeFileTextEnvelope (dir </> "custom-guess-42-datum-42.plutus") Nothing customGuessScript
-  _ <- writeFileTextEnvelope (dir </> "anyone-can-mint.plutus") Nothing apiExamplePlutusMintingScript
-  _ <- writeFileTextEnvelope (dir </> "sum.plutus") Nothing sumScript
-  _ <- writeFileTextEnvelope (dir </> "loop.plutus") Nothing loopScript
-  _ <- writeFileTextEnvelope (dir </> "context-equivalance-test.plutus") Nothing scriptContextCheckScript
-  _ <- writeFileTextEnvelope (dir </> "minting-context-equivalance-test.plutus") Nothing customApiExamplePlutusMintingScript
+  _ <- writeFileTextEnvelope (v1dir </> "always-fails.plutus") Nothing alwaysFailsScript
+  _ <- writeFileTextEnvelope (v1dir </> "always-succeeds-spending.plutus") Nothing alwaysSucceedsScript
+  _ <- writeFileTextEnvelope (v1dir </> "guess-42-datum-42-txin.plutus") Nothing guessScript
+  _ <- writeFileTextEnvelope (v1dir </> "guess-42-stake.plutus") Nothing guessScriptStake
+  _ <- writeFileTextEnvelope (v1dir </> "custom-guess-42-datum-42.plutus") Nothing customGuessScript
+  _ <- writeFileTextEnvelope (v1dir </> "anyone-can-mint.plutus") Nothing apiExamplePlutusMintingScript
+  _ <- writeFileTextEnvelope (v1dir </> "sum.plutus") Nothing sumScript
+  _ <- writeFileTextEnvelope (v1dir </> "loop.plutus") Nothing loopScript
+  _ <- writeFileTextEnvelope (v1dir </> "context-equivalance-test.plutus") Nothing scriptContextTextPayingScript
+  _ <- writeFileTextEnvelope (v1dir </> "minting-context-equivalance-test.plutus") Nothing scriptContextTestMintingScript
+
+
+  _ <- writeFileTextEnvelope (v2dir </> "required-redeemer.plutus") Nothing requireRedeemerScript
+
   return ()
