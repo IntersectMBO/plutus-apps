91aba5581a9b897146d7be248ae8b5edb27a75e6
Andrea Bedini <andrea.bedini@tweag.io>
2022-05-31 10:41:47 +0800

    Make the indexer use plutus-streaming

--- plutus-chain-index/app/Indexer.hs
+++ plutus-chain-index/app/Indexer.hs
@@ -1,140 +1,140 @@
-{-# LANGUAGE ExplicitForAll #-}
-{-# LANGUAGE GADTs          #-}
-{-# LANGUAGE LambdaCase     #-}
-{-# LANGUAGE TupleSections  #-}
+{-# LANGUAGE DerivingVia       #-}
+{-# LANGUAGE ExplicitForAll    #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE TupleSections     #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Main where
 
 import Cardano.Api (Block (Block), BlockHeader (BlockHeader), BlockInMode (BlockInMode), BlockNo (BlockNo), CardanoMode,
-                    ChainPoint (ChainPoint), Hash, NetworkId (Mainnet), SlotNo (SlotNo), chainPointToSlotNo,
-                    deserialiseFromRawBytesHex, proxyToAsType)
-import Cardano.Api qualified as C
-import Cardano.BM.Trace (nullTracer)
-import Cardano.Protocol.Socket.Client (ChainSyncEvent (Resume, RollBackward, RollForward), runChainSync)
-import Control.Concurrent (threadDelay)
+                    ChainPoint (ChainPoint),
+                    EraInMode (AllegraEraInCardanoMode, AlonzoEraInCardanoMode, ByronEraInCardanoMode, MaryEraInCardanoMode, ShelleyEraInCardanoMode),
+                    Hash, IsCardanoEra, NetworkId (Mainnet, Testnet), NetworkMagic (NetworkMagic), SlotNo (SlotNo), Tx,
+                    chainPointToSlotNo, deserialiseFromRawBytesHex, proxyToAsType)
 import Control.Lens.Operators ((^.))
-import Control.Monad (forever, when)
-import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+import Control.Monad (when)
+import Data.ByteString.Char8 qualified as C8
 import Data.List (findIndex)
 import Data.Map (assocs)
 import Data.Maybe (fromJust, fromMaybe)
 import Data.Proxy (Proxy (Proxy))
-import Data.Text (pack)
-import Data.Text.Encoding (encodeUtf8)
+import Data.String (IsString (fromString))
 import Index.VSplit qualified as Ix
 import Ledger.TimeSlot (SlotConfig (..))
 import Ledger.Tx.CardanoAPI (withIsCardanoEra)
 import Marconi.Index.Datum (DatumIndex)
 import Marconi.Index.Datum qualified as Ix
-import Options.Applicative (Parser, execParser, fullDesc, header, help, helper, info, long, metavar, progDesc,
-                            strOption, (<**>))
+import Options.Applicative (Parser, auto, execParser, flag', help, helper, info, long, metavar, option, str, strOption,
+                            (<**>), (<|>))
 import Plutus.ChainIndex.Tx (ChainIndexTx (..))
 import Plutus.Contract.CardanoAPI (fromCardanoTx)
 import Plutus.Script.Utils.V1.Scripts (Datum, DatumHash)
+import Plutus.Streaming (ChainSyncEvent (RollBackward, RollForward), withChainSyncEventStream)
+import Streaming.Prelude qualified as S
 
-{- | This executable is meant to exercise a set of indexers (for now datumhash -> datum)
-     against the mainnet (meant to be used for testing).
-
-     In case you want to access the results of the datumhash indexer you need to query
-     the resulting database:
-     $ sqlite3 datums.sqlite
-     > select slotNo, datumHash, datum from kv_datumhsh_datum where slotNo = 39920450;
-     39920450|679a55b523ff8d61942b2583b76e5d49498468164802ef1ebe513c685d6fb5c2|X(002f9787436835852ea78d3c45fc3d436b324184
--}
-
--- Options
+-- | This executable is meant to exercise a set of indexers (for now datumhash -> datum)
+--     against the mainnet (meant to be used for testing).
+--
+--     In case you want to access the results of the datumhash indexer you need to query
+--     the resulting database:
+--     $ sqlite3 datums.sqlite
+--     > select slotNo, datumHash, datum from kv_datumhsh_datum where slotNo = 39920450;
+--     39920450|679a55b523ff8d61942b2583b76e5d49498468164802ef1ebe513c685d6fb5c2|X(002f9787436835852ea78d3c45fc3d436b324184
 data Options = Options
-  { socketPath :: FilePath
-  , dbPath     :: FilePath
+  { optionsSocketPath   :: String,
+    optionsNetworkId    :: NetworkId,
+    optionsChainPoint   :: ChainPoint,
+    optionsDatabasePath :: FilePath
   }
+  deriving (Show)
 
-options :: Parser Options
-options = Options
-      <$> strOption
-          ( long "socket"
-         <> metavar "SOCKET"
-         <> help "Path to node socket." )
-      <*> strOption
-          ( long "database"
-         <> metavar "DATABASE"
-         <> help "Path to database." )
+parseOptions :: IO Options
+parseOptions = execParser $ info (optionsParser <**> helper) mempty
 
--- We only care about the mainnet
-slotConfig :: SlotConfig
-slotConfig =
-  SlotConfig
-    { scSlotZeroTime = 1596059091000
-    , scSlotLength   = 1000
-    }
+optionsParser :: Parser Options
+optionsParser =
+  Options
+    <$> strOption (long "socket-path" <> help "Path to node socket.")
+    <*> networkIdParser
+    <*> chainPointParser
+    <*> strOption (long "database-path" <> help "Path to database.")
 
-networkId :: NetworkId
-networkId = Mainnet
+networkIdParser :: Parser NetworkId
+networkIdParser =
+  pMainnet <|> pTestnet
+  where
+    pMainnet =
+      flag'
+        Mainnet
+        ( long "mainnet"
+            <> help "Use the mainnet magic id."
+        )
 
--- We don't generally need to sync blocks earlier than the Goguen era (other than
--- testing for memory leaks) so we may want to start synchronising from a slot that
--- is closer to Goguen era.
-closeToGoguen :: ChainPoint
-closeToGoguen =
-  ChainPoint
-    (SlotNo 39795032)
-    (fromJust $ parseHash "3e6f6450f85962d651654ee66091980b2332166f5505fd10b97b0520c9efac90")
+    pTestnet =
+      Testnet . NetworkMagic
+        <$> option
+          auto
+          ( long "testnet-magic"
+              <> metavar "NATURAL"
+              <> help "Specify a testnet magic id."
+          )
 
-parseHash :: String -> Maybe (Hash BlockHeader)
-parseHash hash =
-  deserialiseFromRawBytesHex (proxyToAsType Proxy) (encodeUtf8 $ pack hash)
+chainPointParser :: Parser ChainPoint
+chainPointParser =
+  pure chainPointCloseToGoguen
+    <|> ( ChainPoint
+            <$> option (SlotNo <$> auto) (long "slot-no" <> metavar "SLOT-NO")
+            <*> option str (long "block-hash" <> metavar "BLOCK-HASH")
+        )
+  where
+    -- We don't generally need to sync blocks earlier than the Goguen era (other than
+    -- testing for memory leaks) so we may want to start synchronising from a slot that
+    -- is closer to Goguen era.
+    chainPointCloseToGoguen =
+      ChainPoint
+        (SlotNo 39795032)
+        (fromString "3e6f6450f85962d651654ee66091980b2332166f5505fd10b97b0520c9efac90")
 
 getDatums :: BlockInMode CardanoMode -> [(SlotNo, (DatumHash, Datum))]
 getDatums (BlockInMode (Block (BlockHeader slotNo _ _) txs) era) = withIsCardanoEra era $ concatMap (go era) txs
   where
-    go :: C.IsCardanoEra era
-       => C.EraInMode era C.CardanoMode
-       -> C.Tx era
-       -> [(SlotNo, (DatumHash, Datum))]
+    go ::
+      IsCardanoEra era =>
+      EraInMode era CardanoMode ->
+      Tx era ->
+      [(SlotNo, (DatumHash, Datum))]
     go era' tx =
       let hashes = either (const []) (assocs . _citxData) $ fromCardanoTx era' tx
-      in  map (slotNo,) hashes
-
-processBlock :: IORef DatumIndex -> ChainSyncEvent -> IO ()
-processBlock ixref = \case
-  -- Not really supported
-  Resume point         -> putStrLn ("resume " <> show point) >> pure ()
-  RollForward blk@(BlockInMode (Block (BlockHeader slotNo _ blockNo@(BlockNo b)) _txs) _era) _tip -> do
-    when (b `rem` 1000 == 0) $
-      putStrLn $ show slotNo <> " / " <> show blockNo
-    ix  <- readIORef ixref
-    ix' <- Ix.insert (getDatums blk) ix
-    writeIORef ixref ix'
-  RollBackward point tip -> do
-    putStrLn ("rollback to " <> show tip)
-    rollbackToPoint point ixref
-
-rollbackToPoint
-  :: ChainPoint -> IORef DatumIndex -> IO ()
-rollbackToPoint point ixref = do
-  ix     <- readIORef ixref
-  events <- Ix.getEvents (ix ^. Ix.storage)
-  let ix' = fromMaybe ix $ rollbackOffset events ix
-  writeIORef ixref ix'
-  where
-    rollbackOffset :: [Ix.Event] -> DatumIndex -> Maybe DatumIndex
-    rollbackOffset events ix = do
-      slot   <- chainPointToSlotNo point
-      offset <- findIndex (any (\(s, _) -> s < slot)) events
-      Ix.rewind offset ix
+       in map (slotNo,) hashes
 
 main :: IO ()
 main = do
-  options' <- execParser opts
-  tix <- Ix.open (dbPath options') (Ix.Depth 2160) >>= newIORef
-  _ <- runChainSync (socketPath options')
-                    nullTracer
-                    slotConfig
-                    networkId
-                    [closeToGoguen]
-                    (processBlock tix)
-  forever $ threadDelay 1000000000
-  where
-    opts = info (options <**> helper)
-             ( fullDesc
-            <> progDesc "Synchronise datums with mainnet"
-            <> header "indexer - an indexing proof of concept" )
+  Options {optionsSocketPath, optionsNetworkId, optionsChainPoint, optionsDatabasePath} <- parseOptions
+
+  let initial :: IO DatumIndex
+      initial = Ix.open optionsDatabasePath (Ix.Depth 2160)
+
+      step :: DatumIndex -> ChainSyncEvent (BlockInMode CardanoMode) -> IO DatumIndex
+      step index (RollForward blk@(BlockInMode (Block (BlockHeader slotNo _ blockNo@(BlockNo b)) _txs) _era) _ct) = do
+        when (b `rem` 1000 == 0) $
+          putStrLn $ show slotNo <> " / " <> show blockNo
+        Ix.insert (getDatums blk) index
+      step index (RollBackward cp _ct) = do
+        putStrLn ("rollback to " <> show cp)
+        events <- Ix.getEvents (index ^. Ix.storage)
+        return $
+          fromMaybe index $ do
+            slot <- chainPointToSlotNo cp
+            offset <- findIndex (any (\(s, _) -> s < slot)) events
+            Ix.rewind offset index
+
+      finish :: DatumIndex -> IO ()
+      finish _index = pure () -- Nothing to do here, perhaps we should use this to close the database?
+  withChainSyncEventStream optionsSocketPath optionsNetworkId optionsChainPoint $
+    S.foldM_ step initial finish
+
+-- FIXME Orphan instance. Remove when get access to https://github.com/input-output-hk/cardano-node/pull/3619
+instance IsString (Hash BlockHeader) where
+  fromString = fromJust . deserialiseFromRawBytesHex (proxyToAsType Proxy) . C8.pack
