50cd370430f6a9cb10cf2175f24de3657548b47f
Jordan Millar <jordan.millar@iohk.io>
2022-06-13 12:55:59 -0500

    Create separate directories for v1 and v2 plutus scripts (#486)
    * Reorganize the plutus-example library to distinguish clearly between V1
    and V2 scripts.
    
    * plutus-example reorganization
    create v2 example script
    
    * Modify the ScriptContextChecker module to be more generic
    
    * Propagate the changes to the plutus-example apps and tests
    
    * Update required-redeemer.plutus to also check for an inline datum of 42
    and to check for an inline datum of 42 in the reference inputs

--- plutus-example/test/Test/PlutusExample/ScriptData.hs
+++ plutus-example/test/Test/PlutusExample/ScriptData.hs
@@ -11,6 +11,7 @@ import Data.Aeson qualified as Aeson
 import Hedgehog (Property, forAll, property, tripping, (===))
 import Hedgehog.Internal.Property (failWith)
 
+import PlutusExample.PlutusVersion1.RedeemerContextScripts
 import PlutusExample.ScriptContextChecker
 
 import Test.PlutusExample.Gen
@@ -21,7 +22,7 @@ prop_ScriptData_MyCustomRedeemer =
     myCusRedeem <- forAll genMyCustomRedeemer
     tripping myCusRedeem
              customRedeemerToScriptData
-             customRedeemerFromScriptData
+             (\v -> AnyPV1CustomRedeemer <$> pv1CustomRedeemerFromScriptData v)
 
 prop_ScriptData_MyCustomRedeemer_JSON :: Property
 prop_ScriptData_MyCustomRedeemer_JSON =
@@ -35,9 +36,9 @@ prop_ScriptData_MyCustomRedeemer_JSON =
           Left err ->
             failWith Nothing $ "Failed to decode script data (scriptDataFromJson): " ++ displayError err
           Right sDataDecoded ->
-            case customRedeemerFromScriptData sDataDecoded of
+            case AnyPV1CustomRedeemer <$> pv1CustomRedeemerFromScriptData sDataDecoded of
               Left err ->
-                failWith Nothing $ "Failed to decode custom redeemer(customRedeemerFromScriptData): " ++ err
+                failWith Nothing $ "Failed to decode custom redeemer(pv1CustomRedeemerFromScriptData): " ++ err
               Right cusRedDecoded -> do
                  myCusRedeem === cusRedDecoded
                  sData === Aeson.encode
