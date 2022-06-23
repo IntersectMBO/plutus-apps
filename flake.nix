762f05719203f7de6e73e009ddb75d86b098735b
zeme-iohk <lorenzo.calegari@iohk.io>
2022-06-23 10:11:43 +0200

    added nixConfig to flake

--- flake.nix
+++ flake.nix
@@ -95,4 +95,17 @@
         packages = topLevel.bitte-packages;
         legacyPackages = topLevel;
       }));
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+      "https://hydra.iohk.io"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    allow-import-from-derivation = true;
+    accept-flake-config = true;
+  };
+
 }
