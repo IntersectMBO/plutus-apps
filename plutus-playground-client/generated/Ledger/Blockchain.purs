f61e2d73b60d66b5b16563d9c61b22a956f28810
zmrocze <karolochmanmilarski@gmail.com>
2022-06-26 19:57:35 +0200

    Regenerate purescript.

--- plutus-playground-client/generated/Ledger/Blockchain.purs
+++ plutus-playground-client/generated/Ledger/Blockchain.purs
@@ -17,7 +17,7 @@ import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype, unwrap)
 import Data.Show.Generic (genericShow)
 import Data.Tuple.Nested ((/\))
-import Ledger.Tx.Internal (Tx)
+import Ledger.Tx.Types.Tx (Tx)
 import Type.Proxy (Proxy(Proxy))
 import Data.Argonaut.Decode.Aeson as D
 import Data.Argonaut.Encode.Aeson as E
