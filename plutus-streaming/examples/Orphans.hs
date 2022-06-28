8b5e48e209af85694ce8e53fb49adfdb31235870
Ziyang Liu <unsafeFixIO@gmail.com>
2022-06-27 17:30:23 -0700

    Generate mainnet event dump

--- plutus-streaming/examples/Orphans.hs
+++ plutus-streaming/examples/Orphans.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE FlexibleInstances #-}
-{-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -w #-}
 
 module Orphans where
 
--- shell.nix
+++ shell.nix
@@ -97,6 +97,7 @@ let
     nixpkgs-fmt
     nodejs
     plantuml
+    pre-commit
     shellcheck
     sqlite-interactive
     stack
