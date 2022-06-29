f72836c526bfe0d4a7f9abcf0a0a765b036ca005
Maximilian Algehed <m.algehed@gmail.com>
2022-06-29 09:43:15 +0200

    wip

--- plutus-use-cases/test/Spec/Uniswap.hs
+++ plutus-use-cases/test/Spec/Uniswap.hs
@@ -211,11 +211,11 @@ instance ContractModel UniswapModel where
                 [ (3, createPool) | not . null $ s ^. contractState . exchangeableTokens ] ++
                 [ (10, gen) | gen <- [add True, swap True, remove True, close]
                            , not . null $ s ^. contractState . exchangeableTokens
-                           , not . null $ s ^. contractState . pools ] ++
+                           , not . null $ s ^. contractState . pools ] {-++
                 [ (1, bad) | bad <- [add False, swap False, remove False]
                            , not . null $ s ^. contractState . exchangeableTokens ] ++
                 [ (1, generalRemove) | not . null $ s ^. contractState . exchangeableTokens
-                                     , not . null $ s ^. contractState . pools ]
+                                     , not . null $ s ^. contractState . pools ]-}
     where
       createPool = do
         w <- elements $ wallets \\ [w1]
