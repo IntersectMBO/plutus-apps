bf4ca2ed511428118122915cc2c09c5949849f0c
Evgenii Akentev <i@ak3n.com>
2022-07-21 12:38:06 +0500

    Bump plutus, ledger, node

--- plutus-ledger/src/Ledger/Params.hs
+++ plutus-ledger/src/Ledger/Params.hs
@@ -99,6 +99,7 @@ instance Default ProtocolParameters where
     , protocolParamMaxValueSize = Just 5000
     , protocolParamCollateralPercent = Just 150
     , protocolParamMaxCollateralInputs = Just 3
+    , protocolParamUTxOCostPerByte = Nothing
     }
 
 
