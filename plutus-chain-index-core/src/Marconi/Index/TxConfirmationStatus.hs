63d15148093750e640c0e07338f41255b57102c5
Radu Ometita <radu.ometita@gmail.com>
2022-07-25 13:48:55 +0300

    Store slot numbers in the database

--- plutus-chain-index-core/src/Marconi/Index/TxConfirmationStatus.hs
+++ plutus-chain-index-core/src/Marconi/Index/TxConfirmationStatus.hs
@@ -11,6 +11,7 @@ module Marconi.Index.TxConfirmationStatus
   , Ix.rewind
   ) where
 
+import Cardano.Api (SlotNo (SlotNo))
 import Control.Applicative ((<|>))
 import Control.Lens.Operators ((^.))
 import Data.Foldable (forM_)
@@ -22,7 +23,10 @@ import Data.String (fromString)
 import Database.SQLite.Simple (Only (Only), SQLData (SQLText))
 import Database.SQLite.Simple qualified as SQL
 import Database.SQLite.Simple.FromField (FromField (fromField))
+import Database.SQLite.Simple.FromRow (FromRow (fromRow), field)
 import Database.SQLite.Simple.ToField (ToField (toField))
+import Database.SQLite.Simple.ToRow (ToRow (toRow))
+import GHC.Generics (Generic)
 import Ledger (TxId)
 import Plutus.ChainIndex.Types (BlockNumber (BlockNumber),
                                 TxConfirmedState (TxConfirmedState, blockAdded, timesConfirmed, validity),
@@ -32,7 +36,12 @@ import Index.VSqlite (SqliteIndex)
 import Index.VSqlite qualified as Ix
 
 type Result = Maybe TxConfirmedState
-type Event  = [(TxId, BlockNumber)]
+
+data Event = Event
+  { txId        :: TxId
+  , blockNumber :: BlockNumber
+  , slotNumber  :: SlotNo
+  } deriving (Eq, Show, Generic)
 
 type TCSIndex = SqliteIndex Event () TxId Result
 
@@ -48,6 +57,19 @@ deriving newtype instance FromField BlockNumber
 
 deriving newtype instance ToField BlockNumber
 
+deriving newtype instance FromField SlotNo
+
+deriving newtype instance ToField SlotNo
+
+instance ToRow Event where
+  toRow t = [ toField $ txId t
+            , toField $ blockNumber t
+            , toField $ slotNumber t
+            ]
+
+instance FromRow Event where
+  fromRow = Event <$> field <*> field <*> field
+
 open
   :: FilePath
   -> Depth
@@ -55,7 +77,7 @@ open
 open dbPath (Depth k) = do
   ix <- fromJust <$> Ix.newBoxed query store onInsert k ((k + 1) * 4) dbPath
   let c = ix ^. Ix.handle
-  SQL.execute_ c "CREATE TABLE IF NOT EXISTS tx_status (txId TEXT NOT NULL PRIMARY KEY, blockNo INT NOT NULL)"
+  SQL.execute_ c "CREATE TABLE IF NOT EXISTS tx_status (txId TEXT NOT NULL PRIMARY KEY, blockNo INT NOT NULL, slotNo INT NOT NULL)"
   pure ix
 
 query
@@ -63,15 +85,15 @@ query
   -> TxId
   -> [Event]
   -> IO Result
-query ix txId events = (<|>) <$> searchInMemory
-                             <*> searchOnDisk
+query ix txId' events = (<|>) <$> searchInMemory
+                            <*> searchOnDisk
   where
     searchInMemory :: IO Result
     searchInMemory = do
       buffered <- Ix.getBuffer $ ix ^. Ix.storage
-      let event = find ((== txId) . fst . snd)
-                $ zip [1..] (concat $ events ++ buffered)
-      pure $ event <&> \ (cs, (_, bn)) ->
+      let event = find (\(_, e) -> txId e == txId')
+                $ zip [1..] (events ++ buffered)
+      pure $ event <&> \ (cs, Event _ bn _) ->
         TxConfirmedState { timesConfirmed = Sum    cs
                          , blockAdded     = Last $ Just bn
                          , validity       = Last $ Just TxValid
@@ -79,14 +101,14 @@ query ix txId events = (<|>) <$> searchInMemory
 
     searchOnDisk :: IO Result
     searchOnDisk = do
-      txStatus :: [(TxId, BlockNumber)]
-        <- SQL.query (ix ^. Ix.handle) "SELECT (txId, blockNo) FROM tx_status WHERE txId = ?" (Only txId)
+      txStatus :: [Event]
+        <- SQL.query (ix ^. Ix.handle) "SELECT (txId, blockNo, slotNo) FROM tx_status WHERE txId = ?" (Only txId')
       if null txStatus
          then pure Nothing
-         else let slotNo = snd $ head txStatus
+         else let (Event _ bn _) = head txStatus
               in  pure . Just $
                 TxConfirmedState { timesConfirmed = Sum 0
-                                 , blockAdded     = Last $ Just slotNo
+                                 , blockAdded     = Last $ Just bn
                                  , validity       = Last $ Just TxValid
                                  }
 
@@ -94,11 +116,11 @@ store :: TCSIndex -> IO ()
 store ix = do
   events <- Ix.getEvents $ ix ^. Ix.storage
   buffer <- Ix.getBuffer $ ix ^. Ix.storage
-  let all' = concat $ buffer ++ events
+  let all' = buffer ++ events
       c    = ix ^. Ix.handle
   SQL.execute_ c "BEGIN"
   forM_ all' $
-    SQL.execute c "INSERT INTO tx_status (txId, blockNumber) VALUES (?, ?)"
+    SQL.execute c "INSERT INTO tx_status (txId, blockNumber, slotNo) VALUES (?, ?, ?)"
   SQL.execute_ c "COMMIT"
 
 onInsert :: TCSIndex -> Event -> IO [()]
