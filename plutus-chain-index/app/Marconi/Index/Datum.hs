035b43617a66e8a1e8fb9230fdf768a2d259918b
zeme-iohk <lorenzo.calegari@iohk.io>
2022-07-29 15:06:31 +0200

    Remove old references to hysterical-screams (#635)

--- plutus-chain-index/app/Marconi/Index/Datum.hs
+++ plutus-chain-index/app/Marconi/Index/Datum.hs
@@ -26,8 +26,8 @@ import Database.SQLite.Simple.FromField (FromField (fromField), ResultError (Con
 import Database.SQLite.Simple.ToField (ToField (toField))
 
 import Cardano.Api (SlotNo (SlotNo))
-import Index.VSqlite (SqliteIndex)
-import Index.VSqlite qualified as Ix
+import Plutus.HystericalScreams.Index.VSqlite (SqliteIndex)
+import Plutus.HystericalScreams.Index.VSqlite qualified as Ix
 import Plutus.V1.Ledger.Api (Datum, DatumHash)
 
 type Event        = [(SlotNo, (DatumHash, Datum))]
