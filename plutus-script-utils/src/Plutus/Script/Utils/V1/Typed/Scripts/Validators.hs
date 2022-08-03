764bd9f2862e3165a0d964035bfeb1271ba82ca1
Konstantinos Lambrou-Latreille <konstantinos.lambrou@iohk.io>
2022-08-03 15:33:35 -0400

    Fixed PS generator error in playground

--- plutus-script-utils/src/Plutus/Script/Utils/V1/Typed/Scripts/Validators.hs
+++ plutus-script-utils/src/Plutus/Script/Utils/V1/Typed/Scripts/Validators.hs
@@ -47,7 +47,6 @@ import Plutus.Script.Utils.V1.Typed.Scripts.MonetaryPolicies qualified as MPS
 import Plutus.Script.Utils.V1.Typed.TypeUtils (Any)
 import Plutus.V1.Ledger.Address qualified as PV1
 import Plutus.V1.Ledger.Api qualified as PV1
-import Plutus.V1.Ledger.Tx qualified as PV1
 import PlutusCore.Default (DefaultUni)
 import PlutusTx (CompiledCode, Lift, applyCode, liftCode)
 import PlutusTx.Prelude (check)
@@ -234,8 +233,6 @@ data WrongOutTypeError
 data ConnectionError
   = WrongValidatorAddress PV1.Address PV1.Address
   | WrongOutType WrongOutTypeError
-  | WrongInType PV1.TxInType
-  | MissingInType
   | WrongValidatorType String
   | WrongRedeemerType PV1.BuiltinData
   | WrongDatumType PV1.BuiltinData
@@ -246,8 +243,6 @@ data ConnectionError
 instance Pretty ConnectionError where
   pretty (WrongValidatorAddress a1 a2) = "Wrong validator address. Expected:" <+> pretty a1 <+> "Actual:" <+> pretty a2
   pretty (WrongOutType t)              = "Wrong out type:" <+> viaShow t
-  pretty (WrongInType t)               = "Wrong in type:" <+> viaShow t
-  pretty MissingInType                 = "Missing in type"
   pretty (WrongValidatorType t)        = "Wrong validator type:" <+> pretty t
   pretty (WrongRedeemerType d)         = "Wrong redeemer type" <+> pretty (PV1.builtinDataToData d)
   pretty (WrongDatumType d)            = "Wrong datum type" <+> pretty (PV1.builtinDataToData d)
