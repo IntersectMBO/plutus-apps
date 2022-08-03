d9443bc426fa6b259229a579d8d0a76fcc4d9350
Sjoerd Visscher <sjoerd.visscher@iohk.io>
2022-08-03 11:05:13 +0200

    Remove unused code

--- plutus-ledger/src/Ledger/Typed/Scripts.hs
+++ plutus-ledger/src/Ledger/Typed/Scripts.hs
@@ -10,10 +10,6 @@ module Ledger.Typed.Scripts
   ( module Export
   , TypedScriptTxIn (tyTxInTxIn, tyTxInOutRef)
   , makeTypedScriptTxIn
-  , txInValue
-  , makePubKeyTxIn
-  , typePubKeyTxIn
-  , typeScriptTxIn
   ) where
 
 import Control.Monad.Except (MonadError (throwError))
@@ -38,14 +34,14 @@ instance Eq (DatumType a) => Eq (TypedScriptTxIn a) where
     tyTxInTxIn l == tyTxInTxIn r
       && tyTxInOutRef l == tyTxInOutRef r
 
--- instance (FromJSON (DatumType a), FromData (DatumType a), ToData (DatumType a)) => FromJSON (TypedScriptTxIn a) where
---   parseJSON (Data.Aeson.Object v) =
---     TypedScriptTxIn <$> v .: "tyTxInTxIn" <*> v .: "tyTxInOutRef"
---   parseJSON invalid = typeMismatch "Object" invalid
+instance (FromJSON (DatumType a), FromData (DatumType a), ToData (DatumType a)) => FromJSON (TypedScriptTxIn a) where
+  parseJSON (Data.Aeson.Object v) =
+    TypedScriptTxIn <$> v .: "tyTxInTxIn" <*> v .: "tyTxInOutRef"
+  parseJSON invalid = typeMismatch "Object" invalid
 
--- instance (ToJSON (DatumType a)) => ToJSON (TypedScriptTxIn a) where
---   toJSON TypedScriptTxIn {tyTxInTxIn, tyTxInOutRef} =
---     object ["tyTxInTxIn" .= tyTxInTxIn, "tyTxInOutRef" .= tyTxInOutRef]
+instance (ToJSON (DatumType a)) => ToJSON (TypedScriptTxIn a) where
+  toJSON TypedScriptTxIn {tyTxInTxIn, tyTxInOutRef} =
+    object ["tyTxInTxIn" .= tyTxInTxIn, "tyTxInOutRef" .= tyTxInOutRef]
 
 -- | Create a 'TypedScriptTxIn' from a correctly-typed validator, redeemer, and output ref.
 makeTypedScriptTxIn ::
@@ -63,54 +59,3 @@ makeTypedScriptTxIn lang si r tyRef =
       ds = Datum (toBuiltinData d)
       txInT = ConsumeScriptAddress lang vs rs ds
    in TypedScriptTxIn @inn (TxIn (Export.tyTxOutRefRef tyRef) (Just txInT)) tyRef
-
-txInValue :: TypedScriptTxIn a -> Value
-txInValue = txOutValue . tyTxOutTxOut . tyTxOutRefOut . tyTxInOutRef
-
--- | A public-key 'TxIn'. We need this to be sure that it is not a script input.
-newtype PubKeyTxIn = PubKeyTxIn {unPubKeyTxIn :: TxIn}
-  deriving stock (Eq, Generic)
-  deriving newtype (FromJSON, ToJSON)
-
--- | Create a 'PubKeyTxIn'.
-makePubKeyTxIn :: TxOutRef -> PubKeyTxIn
-makePubKeyTxIn ref = PubKeyTxIn . TxIn ref . Just $ ConsumePublicKeyAddress
-
--- | Create a 'PubKeyTxIn' from an existing 'TxIn' by checking that it has the right payment type.
-typePubKeyTxIn ::
-  forall m.
-  (MonadError ConnectionError m) =>
-  TxIn ->
-  m PubKeyTxIn
-typePubKeyTxIn txIn =
-  case txInType txIn of
-    Just ConsumePublicKeyAddress -> pure $ PubKeyTxIn txIn
-    Just _                       -> throwError MissingInType -- $ WrongInType x -- TODO
-    Nothing                      -> throwError MissingInType
-
--- | Create a 'TypedScriptTxIn' from an existing 'TxIn' by checking the types of its parts.
-typeScriptTxIn ::
-  forall inn m.
-  ( FromData (RedeemerType inn),
-    ToData (RedeemerType inn),
-    FromData (DatumType inn),
-    ToData (DatumType inn),
-    MonadError ConnectionError m
-  ) =>
-  (TxOutRef -> Maybe (TxOut, Datum)) ->
-  TypedValidator inn ->
-  TxIn ->
-  m (TypedScriptTxIn inn)
-typeScriptTxIn lookupRef typedValidator txIn =
-  case txInType txIn of
-    Just (ConsumeScriptAddress lang _val re da) -> do
-      rsVal <- checkRedeemer typedValidator re
-      _ <- checkDatum typedValidator da
-      let txOutRef = txInRef txIn
-      case lookupRef txOutRef of
-        Just (txOut, datum) -> do
-          typedOut <- typeScriptTxOutRef @inn typedValidator txOutRef txOut datum
-          pure $ makeTypedScriptTxIn lang typedValidator rsVal typedOut
-        Nothing -> throwError UnknownRef
-    Just _ -> throwError MissingInType -- $ WrongInType x -- TODO
-    Nothing -> throwError MissingInType
