c102e7167804a647701f017531b54485c34d1762
James Browning <james.browning@iohk.io>
2022-08-04 11:02:32 +0100

    Change MustSpendAtLeast to use Datum and included phase-2 failure test

--- plutus-contract/test/Spec/MustSpendAtLeast.hs
+++ plutus-contract/test/Spec/MustSpendAtLeast.hs
@@ -9,8 +9,6 @@
 module Spec.MustSpendAtLeast(tests) where
 
 import Control.Monad (void)
-import Data.Map qualified as Map
-import Data.Void (Void)
 import Test.Tasty (TestTree, testGroup)
 
 import Ledger qualified
@@ -18,18 +16,18 @@ import Ledger.Ada qualified as Ada
 import Ledger.Constraints.OffChain qualified as Constraints (MkTxError (OwnPubKeyMissing), plutusV1OtherScript,
                                                              plutusV1TypedValidatorLookups, unspentOutputs)
 import Ledger.Constraints.OnChain.V1 qualified as Constraints (checkScriptContext)
-import Ledger.Constraints.TxConstraints qualified as Constraints (mustIncludeDatum, mustPayToTheScript,
-                                                                  mustSpendAtLeast, mustSpendScriptOutput)
+import Ledger.Constraints.TxConstraints qualified as Constraints (collectFromTheScript, mustIncludeDatum,
+                                                                  mustPayToTheScript, mustSpendAtLeast)
 import Ledger.Tx qualified as Tx
 import Ledger.Typed.Scripts qualified as Scripts
 import Plutus.Contract as Con
-import Plutus.Contract.Test (assertContractError, assertValidatedTransactionCount, checkPredicateOptions,
-                             defaultCheckOptions, w1, (.&&.))
+import Plutus.Contract.Test (assertContractError, assertFailedTransaction, assertValidatedTransactionCount,
+                             checkPredicateOptions, defaultCheckOptions, w1, (.&&.))
 import Plutus.Trace qualified as Trace
 import Plutus.V1.Ledger.Api (Datum (Datum), ScriptContext, Validator, ValidatorHash)
-import Plutus.V1.Ledger.Scripts (unitRedeemer)
+import Plutus.V1.Ledger.Scripts (ScriptError (EvaluationError))
 import PlutusTx qualified
-import PlutusTx.Prelude as P (traceError)
+import PlutusTx.Prelude qualified as P
 import Prelude hiding (not)
 
 tests :: TestTree
@@ -38,32 +36,33 @@ tests =
         [ entireScriptBalance
         , lessThanScriptBalance
         , higherThanScriptBalance
+        , phase2Failure
         ]
 
 scriptBalance :: Integer
 scriptBalance = 25_000_000
 
-contract :: Integer -> Contract () Empty ContractError ()
-contract amt = do
-    let lookups1 = Constraints.plutusV1TypedValidatorLookups $ typedValidator amt
-        tx1 = Constraints.mustPayToTheScript () (Ada.lovelaceValueOf scriptBalance)
+contract :: Integer -> Integer -> Contract () Empty ContractError ()
+contract offAmt onAmt = do
+    let lookups1 = Constraints.plutusV1TypedValidatorLookups typedValidator
+        tx1 = Constraints.mustPayToTheScript onAmt (Ada.lovelaceValueOf scriptBalance)
     ledgerTx1 <- submitTxConstraintsWith lookups1 tx1
     awaitTxConfirmed $ Tx.getCardanoTxId ledgerTx1
-    utxos <- utxosAt $ scrAddress amt
-    let orefs = fst <$> Map.toList utxos
-        lookups2 =
-            Constraints.plutusV1OtherScript (validatorScript amt)
+
+    utxos <- utxosAt scrAddress
+    let lookups2 = Constraints.plutusV1TypedValidatorLookups typedValidator
+            <> Constraints.plutusV1OtherScript validatorScript
             <> Constraints.unspentOutputs utxos
         tx2 =
-            foldMap (\oref -> Constraints.mustSpendScriptOutput oref unitRedeemer) orefs
-            <> Constraints.mustIncludeDatum (Datum $ PlutusTx.toBuiltinData amt)
-            <> Constraints.mustSpendAtLeast (Ada.lovelaceValueOf amt)
-    ledgerTx2 <- submitTxConstraintsWith @Void lookups2 tx2
+            Constraints.collectFromTheScript utxos ()
+            <> Constraints.mustIncludeDatum (Datum $ PlutusTx.toBuiltinData onAmt)
+            <> Constraints.mustSpendAtLeast (Ada.lovelaceValueOf offAmt)
+    ledgerTx2 <- submitTxConstraintsWith @UnitTest lookups2 tx2
     awaitTxConfirmed $ Tx.getCardanoTxId ledgerTx2
 
-trace :: Integer -> Trace.EmulatorTrace ()
-trace amt = do
-    void $ Trace.activateContractWallet w1 $ contract amt
+trace :: Integer -> Integer -> Trace.EmulatorTrace ()
+trace offAmt onAmt = do
+    void $ Trace.activateContractWallet w1 $ contract offAmt onAmt
     void $ Trace.waitNSlots 1
 
 entireScriptBalance :: TestTree
@@ -71,7 +70,7 @@ entireScriptBalance = checkPredicateOptions
     defaultCheckOptions
     "Successful use of mustSpendAtLeast at script's exact balance"
     (assertValidatedTransactionCount 2)
-    (void $ trace scriptBalance)
+    (void $ trace scriptBalance scriptBalance)
 
 lessThanScriptBalance :: TestTree
 lessThanScriptBalance =
@@ -80,37 +79,49 @@ lessThanScriptBalance =
             defaultCheckOptions
             "Successful use of mustSpendAtLeast below script's balance"
             (assertValidatedTransactionCount 2)
-            (void $ trace amt)
+            (void $ trace amt amt )
 
-higherThanScriptBalance :: TestTree -- This should fail with a more accurate contract error than "OwnPubKeyMissing"
+higherThanScriptBalance :: TestTree
 higherThanScriptBalance =
     let amt = scriptBalance + 1
     in  checkPredicateOptions
             defaultCheckOptions
             "Fail validation when mustSpendAtLeast is greater than script's balance"
-            (assertContractError (contract amt) (Trace.walletInstanceTag w1) (\case { ConstraintResolutionContractError Constraints.OwnPubKeyMissing -> True; _ -> False }) "failed to throw error"
+            (assertContractError (contract amt amt) (Trace.walletInstanceTag w1) (\case { ConstraintResolutionContractError Constraints.OwnPubKeyMissing -> True; _ -> False }) "failed to throw error"
             .&&. assertValidatedTransactionCount 1)
-            (void $ trace amt)
+            (void $ trace amt amt)
+
+phase2Failure :: TestTree
+phase2Failure =
+    let offAmt = scriptBalance
+        onAmt  = scriptBalance + 1
+    in  checkPredicateOptions
+            defaultCheckOptions
+            "Fail phase-2 validation when on-chain mustSpendAtLeast is greater than script's balance"
+            (assertFailedTransaction (\_ err _ -> case err of {Ledger.ScriptFailure (EvaluationError ("L5":_) _) -> True; _ -> False }))
+            (void $ trace offAmt onAmt)
 
 {-# INLINEABLE mkValidator #-}
-mkValidator :: Integer -> () -> () -> ScriptContext -> Bool
-mkValidator amt _ _ ctx = Constraints.checkScriptContext @Void @Void (Constraints.mustSpendAtLeast (Ada.lovelaceValueOf amt)) ctx || P.traceError "mustSpendAtLeast not satisfied"
+mkValidator :: Integer -> () -> ScriptContext -> Bool
+mkValidator amt _ ctx = P.traceIfFalse "mustSpendAtLeast not satisfied" (Constraints.checkScriptContext @() @() (Constraints.mustSpendAtLeast P.$ Ada.lovelaceValueOf amt) ctx)
 
 data UnitTest
-instance Scripts.ValidatorTypes UnitTest
+instance Scripts.ValidatorTypes UnitTest where
+    type instance DatumType UnitTest = Integer
+    type instance RedeemerType UnitTest = ()
 
-typedValidator :: Integer -> Scripts.TypedValidator UnitTest
-typedValidator = Scripts.mkTypedValidatorParam @UnitTest
+typedValidator :: Scripts.TypedValidator UnitTest
+typedValidator = Scripts.mkTypedValidator @UnitTest
     $$(PlutusTx.compile [||mkValidator||])
     $$(PlutusTx.compile [|| wrap ||])
     where
         wrap = Scripts.mkUntypedValidator
 
-validatorScript :: Integer -> Validator
-validatorScript = Scripts.validatorScript . typedValidator
+validatorScript :: Validator
+validatorScript = Scripts.validatorScript typedValidator
 
-valHash :: Integer -> ValidatorHash
-valHash = Scripts.validatorHash . typedValidator
+valHash :: ValidatorHash
+valHash = Scripts.validatorHash typedValidator
 
-scrAddress :: Integer -> Ledger.Address
-scrAddress = Ledger.scriptHashAddress . valHash
+scrAddress :: Ledger.Address
+scrAddress = Ledger.scriptHashAddress valHash
