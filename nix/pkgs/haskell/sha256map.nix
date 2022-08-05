0f35d5666e53b8f9f1bf89671c21eed71ea18ead
Evgenii Akentev <i@ak3n.com>
2022-08-05 12:36:51 +0400

    Update cardano-node to 1.35.3-rc1 with deps

--- nix/pkgs/haskell/sha256map.nix
+++ nix/pkgs/haskell/sha256map.nix
@@ -4,8 +4,8 @@
   "https://github.com/input-output-hk/cardano-base"."0f3a867493059e650cda69e20a5cbf1ace289a57" = "0p0az3sbkhb7njji8xxdrfb0yx2gc8fmrh872ffm8sfip1w29gg1";
   "https://github.com/input-output-hk/cardano-config"."1646e9167fab36c0bff82317743b96efa2d3adaa" = "sha256-TNbpnR7llUgBN2WY7CryMxNVupBIUH01h1hRNHoxboY=";
   "https://github.com/input-output-hk/cardano-crypto"."f73079303f663e028288f9f4a9e08bcca39a923e" = "1n87i15x54s0cjkh3nsxs4r1x016cdw1fypwmr68936n3xxsjn6q";
-  "https://github.com/input-output-hk/cardano-ledger"."ebcf1a8936dd84de0182d54004473f4ce66c7923" = "1nx07kcjhj39alarr0bxw9viw3m6flfr8d14g2a3crymf6hxwg36";
-  "https://github.com/input-output-hk/cardano-node"."1.35.2" = "01a5qdrmsag18s2mlf8axfbrag59j2fp6xyc89pwmzgs7x77ldsr";
+  "https://github.com/input-output-hk/cardano-ledger"."c7c63dabdb215ebdaed8b63274965966f2bf408f" = "1nx07kcjhj39alarr0bxw9viw3m6flfr8d14g2a3crymf6hxwg36";
+  "https://github.com/input-output-hk/cardano-node"."1.35.3-rc1" = "01a5qdrmsag18s2mlf8axfbrag59j2fp6xyc89pwmzgs7x77ldsr";
   "https://github.com/input-output-hk/cardano-prelude"."bb4ed71ba8e587f672d06edf9d2e376f4b055555" = "00h10l5mmiza9819p9v5q5749nb9pzgi20vpzpy1d34zmh6gf1cj";
   "https://github.com/input-output-hk/cardano-wallet"."5c77b286a412fb3795bdf8755888d0c4236d648c" = "sha256-lxpPV7TG40ilCaQzKBc+DqXPABTjkHJGxAo+TCLmNLk=";
   "https://github.com/input-output-hk/goblins"."cde90a2b27f79187ca8310b6549331e59595e7ba" = "17c88rbva3iw82yg9srlxjv2ia5wjb9cyqw44hik565f5v9svnyg";
@@ -14,7 +14,7 @@
   "https://github.com/input-output-hk/io-sim"."57e888b1894829056cb00b7b5785fdf6a74c3271" = "sha256-TviSvCBEYtlKEo9qJmE8pCE25nMjDi8HeIAFniunaM8=";
   "https://github.com/input-output-hk/optparse-applicative"."7497a29cb998721a9068d5725d49461f2bba0e7a" = "1gvsrg925vynwgqwplgjmp53vj953qyh3wbdf34pw21c8r47w35r";
   "https://github.com/input-output-hk/ouroboros-network"."cb9eba406ceb2df338d8384b35c8addfe2067201" = "066llskxzjgcs13lwlvklb28azb9kd9b77j61x8fvrj1rlf5njfw";
-  "https://github.com/input-output-hk/plutus"."a3fba9f4e776c38bc54cb9a1c1cae82d2338b718" = "sha256-sI3tnSWqgYIvGg9J4VP1aMsvBCst2+cmh2/JgjN3xO8=";
+  "https://github.com/input-output-hk/plutus"."a56c96598b4b25c9e28215214d25189331087244" = "sha256-sI3tnSWqgYIvGg9J4VP1aMsvBCst2+cmh2/JgjN3xO8=";
   "https://github.com/input-output-hk/purescript-bridge"."47a1f11825a0f9445e0f98792f79172efef66c00" = "0da1vn2l6iyfxcjk58qal1l4755v92zi6yppmjmqvxf1gacyf9px";
   "https://github.com/input-output-hk/servant-purescript"."44e7cacf109f84984cd99cd3faf185d161826963" = "10pb0yfp80jhb9ryn65a4rha2lxzsn2vlhcc6xphrrkf4x5lhzqc";
   "https://github.com/input-output-hk/typed-protocols"."181601bc3d9e9d21a671ce01e0b481348b3ca104" = "sha256-5Wof5yTKb12EPY6B8LfapX18xNZZpF+rvhnQ88U6KdM=";
