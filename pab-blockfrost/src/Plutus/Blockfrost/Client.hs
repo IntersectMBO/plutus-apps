c0453004426a62e42ec30749beaabb52be7528a2
Osi_Tomás <63505494+TomasOsiecki@users.noreply.github.com>
2022-09-14 11:07:19 -0300

    PAB <> Blockfrost integration (#655)
    * Adds pab-blockfrost module and getTip endpoint
    
    * Adds DatumFromHash endpoint
    
    * Adds RedeemerFromHash endpoint
    
    * Adds ValidatorFromHash endpoint
    
    * Adds MintingPolicyFromHash and StakeValidatorFromHash endpoints
    
    * Adds UnspentTxOutFromRef endpoitn
    
    * Adds UtxoSetMembership endpoint
    
    * Updates PAB to use Plutus.Blockfrost module
    
    * Adds UtxoSetAtAddress endpoint
    
    * Adds UtxoSetWithCurrency endpoint
    
    * Updates Readme with integration instructions
    
    * Fixes BlockfrostNotFound error on empty wallets
    
    * Adds default response for IsUtxo endpoint
    
    * Fixed UnspentTxOut Endpoint and removed warnings
    
    * Fixes bug on IsUtxo endpoint and removes warnings
    
    * Removes warnings and QQ from textToDatumHash
    
    * Removes QQ from ProcessTip and linter hints
    
    * Removes QQ from Responses module
    
    * Adds unspentTxOutsAt endpoint to PAB
    
    * Fixes warnings at PAB.Blockfrost.Responses
    
    * Adds new configuration types to PAB.
    
    * Adds blockfrost types to .cabal
    
    * Adds configuration support for Blockfrost inside PAB
    
    * Renames chainIndexConfig to chainQueryConfig
    
    * Adds ChainQueryEnv type to AppEnv
    
    * Adds txoSetAtAddress endpoint to blockfrost
    
    * Modifies UnspentTxOutFromRef endpoint and adds TxOutFromRef endpoint
    
    * Adds TxFromTxId endpoint
    
    * Add TxsFromTxIds endpoint
    
    * Adds first iteration of pagination support
    
    Co-authored-by: Osi_Tomás <TomasOsiecki@users.noreply.github.com>
    Co-authored-by: Valentino Cerutti <Micrograx@users.noreply.github.com>
    Co-authored-by: Ale Gadea <73836359+alegadea@users.noreply.github.com>

---
+++ pab-blockfrost/src/Plutus/Blockfrost/Client.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs            #-}
+{-# LANGUAGE TypeOperators    #-}
+
+module Plutus.Blockfrost.Client(handleBlockfrostClient) where
+
+import Blockfrost.Client (BlockfrostClientT, BlockfrostError (BlockfrostNotFound), projectFromFile, runBlockfrost)
+import Control.Monad.Freer (Eff, LastMember, Member, type (~>))
+import Control.Monad.Freer.Reader (Reader, ask)
+import Control.Monad.IO.Class (MonadIO (..))
+import Plutus.ChainIndex.Effects (ChainIndexQueryEffect (..))
+
+import Plutus.Blockfrost.Queries
+import Plutus.Blockfrost.Responses
+import Plutus.Blockfrost.Types (BlockfrostEnv (..))
+import Plutus.Blockfrost.Utils
+
+
+-- | Handle 'ChainIndexQueryEffect' by making HTTP calls to the remote
+--   blockfrost server.
+handleBlockfrostClient ::
+    forall m effs.
+    ( LastMember m effs
+    , Member (Reader BlockfrostEnv) effs
+    , MonadIO m
+    )
+    => ChainIndexQueryEffect
+    ~> Eff effs
+handleBlockfrostClient event = do
+    bfEnv <- ask
+    liftIO $ do
+        prj <- projectFromFile $ envBfTokenPath bfEnv
+        let
+            runClient :: forall a. BlockfrostClientT IO a -> IO a
+            runClient a = runBlockfrost prj a >>= either (ioError . userError . show) return
+
+            runClientWithDef :: forall a. BlockfrostClientT IO a -> BlockfrostClientT IO a -> IO a
+            runClientWithDef defIO a = do
+                response <- runBlockfrost prj a
+                case response of
+                    Right a'                -> return a'
+                    Left BlockfrostNotFound -> runClient defIO
+                    Left e                  -> ioError (userError $ show e)
+
+            runClientMaybe :: forall a. BlockfrostClientT IO a -> IO (Maybe a)
+            runClientMaybe a = do
+                response <- runBlockfrost prj a
+                case response of
+                    Right a'                -> pure (Just a')
+                    Left BlockfrostNotFound -> pure Nothing
+                    Left e                  -> ioError (userError $ show e)
+
+        case event of
+            DatumFromHash d               -> (runClientMaybe . getDatumBlockfrost . toBlockfrostDatumHash) d        >>= processGetDatum
+            RedeemerFromHash d            -> (runClientMaybe . getDatumBlockfrost . toBlockfrostDatumHash) d        >>= processGetDatum
+            ValidatorFromHash d           -> (runClientMaybe . getValidatorBlockfrost . toBlockfrostScriptHash) d   >>= processGetValidator
+            MintingPolicyFromHash d       -> (runClientMaybe . getValidatorBlockfrost . toBlockfrostScriptHash) d   >>= processGetValidator
+            StakeValidatorFromHash d      -> (runClientMaybe . getValidatorBlockfrost . toBlockfrostScriptHash) d   >>= processGetValidator
+            UnspentTxOutFromRef r         -> (runClientMaybe . getUnspentTxOutBlockfrost . toBlockfrostRef) r       >>= processUnspentTxOut
+            TxOutFromRef r                -> (runClientMaybe . getTxOutBlockfrost . toBlockfrostRef) r              >>= processUnspentTxOut
+            TxFromTxId i                  -> (runClientMaybe . getTxFromTxIdBlockfrost . toBlockfrostTxHash) i      >>= processGetTxFromTxId
+            TxsFromTxIds is               -> (runClientWithDef defaultGetList . getTxsFromTxIdsBlockfrost . toBlockfrostTxHashes) is >>= processGetTxsFromTxIds
+            UtxoSetMembership r           -> (runClientWithDef defaultIsUtxo  . getIsUtxoBlockfrost . toBlockfrostRef) r                >>= processIsUtxo
+            UtxoSetAtAddress pq a         -> (runClientWithDef defaultGetUtxo . getUtxoAtAddressBlockfrost pq . credentialToAddress (envNetworkId bfEnv)) a  >>= processGetUtxos pq
+            UtxoSetWithCurrency pq a      -> (runClientWithDef defaultGetUtxo . getUtxoSetWithCurrency pq . toBlockfrostAssetId) a      >>= processGetUtxos pq
+            TxoSetAtAddress pq a          -> (runClientWithDef defaultGetList . getTxoAtAddressBlockfrost pq . credentialToAddress (envNetworkId bfEnv)) a >>= processGetTxos pq
+            GetTip                        -> runClient getTipBlockfrost >>= processTip
+            UnspentTxOutSetAtAddress pq a -> (runClientWithDef defaultGetList . getUnspentAtAddressBlockfrost pq . credentialToAddress (envNetworkId bfEnv)) a  >>= processUnspentTxOutSetAtAddress pq a
