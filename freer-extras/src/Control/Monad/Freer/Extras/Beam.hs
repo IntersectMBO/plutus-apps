935562d420c3809427acc903227b645d2dbb00a9
MartorSkull <martin_montane_6@hotmail.com>
2022-09-19 16:26:15 -0300

    Documentation and example config

--- freer-extras/src/Control/Monad/Freer/Extras/Beam.hs
+++ freer-extras/src/Control/Monad/Freer/Extras/Beam.hs
@@ -20,9 +20,9 @@
 {-# LANGUAGE ViewPatterns          #-}
 
 module Control.Monad.Freer.Extras.Beam (
-      module Control.Monad.Freer.Extras.Beam.Postgres,
+      module Control.Monad.Freer.Extras.Beam.Effects,
       module Control.Monad.Freer.Extras.Beam.Common
 ) where
 
 import Control.Monad.Freer.Extras.Beam.Common
-import Control.Monad.Freer.Extras.Beam.Postgres
+import Control.Monad.Freer.Extras.Beam.Effects
