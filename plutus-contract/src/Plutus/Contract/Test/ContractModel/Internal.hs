eec7a1b732bec940638ba7b3f3ef87e68f5f43f2
Evgenii Akentev <i@ak3n.com>
2022-10-05 15:02:16 +0400

    seems to work

--- plutus-contract/src/Plutus/Contract/Test/ContractModel/Internal.hs
+++ plutus-contract/src/Plutus/Contract/Test/ContractModel/Internal.hs
@@ -993,7 +993,7 @@ toStateModelActions :: ContractModel state =>
                         Actions state -> StateModel.Actions (ModelState state)
 toStateModelActions (Actions_ rs (Smart k s)) =
   StateModel.Actions_ rs (Smart k $ map mkStep s)
-    where mkStep (ActWaitUntil v n) = v := (WaitUntil $ n + 1)
+    where mkStep (ActWaitUntil v n) = v := WaitUntil n
           mkStep act                = varOf act := ContractAction (isBind act) (actionOf act)
 
 fromStateModelActions :: StateModel.Actions (ModelState s) -> Actions s
