2e80a11022e9ecbc36907cd2e96163bb783a8898
zeme-iohk <lorenzo.calegari@iohk.io>
2022-11-10 14:04:18 +0100

    WIP

--- nix/modules/web-ghc.nix
+++ nix/modules/web-ghc.nix
@@ -57,6 +57,7 @@ in
         # allow binding on port 80
         AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
 
+
         # sane defaults for security
         DynamicUser = true;
         ProtectKernelTunables = true;
