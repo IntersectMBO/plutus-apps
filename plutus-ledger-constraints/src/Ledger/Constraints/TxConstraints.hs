0cb1c8325f9bd3bd810d3d306ef016ca07f07da7
Sjoerd Visscher <sjoerd.visscher@iohk.io>
2022-11-16 17:48:58 +0100

    Some clean up

--- plutus-ledger-constraints/src/Ledger/Constraints/TxConstraints.hs
+++ plutus-ledger-constraints/src/Ledger/Constraints/TxConstraints.hs
@@ -1025,27 +1025,3 @@ collectFromPlutusV2ScriptFilter flt am vls red = -- (Redeemer red) =
     let mp'  = fromMaybe Haskell.mempty $ am ^. at (PV2.mkValidatorAddress vls)
         ourUtxo = Map.filterWithKey flt mp'
     in foldMap (flip mustSpendScriptOutput red) $ Map.keys ourUtxo
-
--- TODO Uncomment and modify once PlutusV2 TypedValidator are available
--- -- | Given the pay to script address of the 'Validator', collect from it
--- --   all the outputs that match a predicate, using the 'RedeemerValue'.
--- collectFromTheScriptFilter ::
---     forall i o
---     .  (TxOutRef -> DecoratedTxOut -> Bool)
---     -> Map.Map TxOutRef DecoratedTxOut
---     -> i
---     -> TxConstraints i o
--- collectFromTheScriptFilter flt utxo red =
---     let ourUtxo :: Map.Map TxOutRef DecoratedTxOut
---         ourUtxo = Map.filterWithKey flt utxo
---     in collectFromTheScript ourUtxo red
-
--- -- | A version of 'collectFromScript' that selects all outputs
--- --   at the address
--- collectFromTheScript ::
---     forall i o
---     .  Map.Map TxOutRef DecoratedTxOut
---     -> i
---     -> TxConstraints i o
--- collectFromTheScript utxo redeemer =
---     foldMap (flip mustSpendOutputFromTheScript redeemer) $ Map.keys utxo
