0564831c402cc8bd6ffc7f0d39a7296d751bec36
Nicolas Biri <nicolas.biri@iohk.io>
2022-11-24 08:35:31 +0100

    cleaner Tx.Constraint test

--- plutus-contract/test/Spec/Contract/Tx/Constraints/RequiredSigner.hs
+++ plutus-contract/test/Spec/Contract/Tx/Constraints/RequiredSigner.hs
@@ -7,7 +7,6 @@
 {-# LANGUAGE TypeFamilies        #-}
 module Spec.Contract.Tx.Constraints.RequiredSigner(tests) where
 
-import Control.Lens (_Just, has)
 import Control.Monad (void)
 import Data.Void (Void)
 import Test.Tasty (TestTree, testGroup)
@@ -20,18 +19,18 @@ import Ledger qualified
 import Ledger.Ada qualified as Ada
 import Ledger.CardanoWallet qualified as CW
 import Ledger.Constraints.OffChain qualified as Constraints hiding (requiredSignatories)
-import Ledger.Constraints.OnChain.V1 qualified as Constraints
+import Ledger.Constraints.OnChain.V2 qualified as Constraints
 import Ledger.Constraints.TxConstraints qualified as Constraints
-import Ledger.Test (someAddressV2, someValidatorHashV2, someValidatorV2)
 import Ledger.Tx qualified as Tx
 import Ledger.Tx.Constraints qualified as TxCons
-import Ledger.Typed.Scripts qualified as Scripts
 import Plutus.Contract as Con
 import Plutus.Contract.Test (assertFailedTransaction, assertValidatedTransactionCount, changeInitialWalletValue,
                              checkPredicateOptions, defaultCheckOptions, mockWalletPaymentPubKeyHash, w1, w2)
 import Plutus.Script.Utils.V2.Typed.Scripts as PSU.V2
+import Plutus.Script.Utils.V2.Typed.Scripts qualified as Scripts
 import Plutus.Trace qualified as Trace
 import Plutus.V1.Ledger.Scripts (ScriptError (EvaluationError), unitDatum)
+import Plutus.V2.Ledger.Api qualified as PV2
 import PlutusTx qualified
 import Prelude
 import Wallet.Emulator.Wallet (signPrivateKeys, walletToMockWallet)
@@ -151,7 +150,7 @@ instance Scripts.ValidatorTypes UnitTest  where
     type instance RedeemerType UnitTest = Ledger.PaymentPubKeyHash
 
 {-# INLINEABLE mustBeSignedByValidator #-}
-mustBeSignedByValidator :: () -> Ledger.PaymentPubKeyHash -> Ledger.ScriptContext -> Bool
+mustBeSignedByValidator :: () -> Ledger.PaymentPubKeyHash -> PV2.ScriptContext -> Bool
 mustBeSignedByValidator _ pkh = Constraints.checkScriptContext @Void @Void (Constraints.mustBeSignedBy pkh)
 
 mustBeSignedByTypedValidator :: Scripts.TypedValidator UnitTest
@@ -174,44 +173,36 @@ cardanoTxOwnWalletContract
     :: Ledger.PaymentPubKeyHash
     -> Ledger.PaymentPubKeyHash
     -> Contract () EmptySchema ContractError ()
-cardanoTxOwnWalletContract pk pkh = do
+cardanoTxOwnWalletContract paymentPubKey signedPubKey = do
     let mkTx lookups constraints = either (error . show) id $ TxCons.mkTx @UnitTest def lookups constraints
 
     utxos <- ownUtxos
-    myAddr <- ownAddress
     let get3 (a:b:c:_) = (a, b, c)
         get3 _         = error "Spec.Contract.TxConstraints.get3: not enough inputs"
         ((utxoRef, utxo), (utxoRefForBalance1, _), (utxoRefForBalance2, _)) = get3 $ M.toList utxos
-        lookups1 = Constraints.unspentOutputs utxos
-               <> Constraints.typedValidatorLookups mustBeSignedByTypedValidator
-               <> Constraints.plutusV2OtherScript someValidatorV2
-               <> Constraints.paymentPubKeyHash pk
-        tx1 = Constraints.mustPayToOtherScriptWithDatumInTx
-                someValidatorHashV2
-                (Ledger.Datum $ PlutusTx.toBuiltinData utxoRef)
-                (Ada.adaValueOf 5)
+        lookups1 = Constraints.typedValidatorLookups mustBeSignedByTypedValidator
+                <> Constraints.unspentOutputs utxos
+        tx1 = Constraints.mustPayToTheScriptWithDatumInTx
+                ()
+                (Ada.lovelaceValueOf 25_000_000)
           <> Constraints.mustSpendPubKeyOutput utxoRefForBalance1
           <> Constraints.mustUseOutputAsCollateral utxoRefForBalance1
-          <> Constraints.mustPayToAddressWithReferenceValidator
-                myAddr
-                someValidatorHashV2
-                Nothing
-                (Ada.adaValueOf 30)
-    submitTxConfirmed $ mkTx lookups1 tx1
+    ledgerTx1 <- submitTxConstraintsWith lookups1 tx1
+    awaitTxConfirmed $ Tx.getCardanoTxId ledgerTx1
 
     -- Trying to unlock the Ada in the script address
-    scriptUtxos <- utxosAt someAddressV2
+    scriptUtxos <- utxosAt (Ledger.scriptHashAddress $ Scripts.validatorHash mustBeSignedByTypedValidator)
     utxos' <- ownUtxos
-    let
-        scriptUtxo = fst . head . M.toList $ scriptUtxos
-        refScriptUtxo = head . M.keys . M.filter (has $ Tx.decoratedTxOutReferenceScript . _Just) $ utxos'
-        lookups2 = Constraints.unspentOutputs (M.singleton utxoRef utxo <> scriptUtxos <> utxos')
-          <> Constraints.typedValidatorLookups mustBeSignedByTypedValidator
-        tx2 = Constraints.mustReferenceOutput utxoRef
-          <> Constraints.mustSpendScriptOutputWithReference scriptUtxo Ledger.unitRedeemer refScriptUtxo
-          <> Constraints.mustSpendPubKeyOutput utxoRefForBalance2
-          <> Constraints.mustUseOutputAsCollateral utxoRefForBalance2
-          <> Constraints.mustBeSignedBy pkh
+    let lookups2 =
+            Constraints.typedValidatorLookups mustBeSignedByTypedValidator
+            <> Constraints.unspentOutputs (M.singleton utxoRef utxo <> scriptUtxos <> utxos')
+            <> Constraints.paymentPubKeyHash paymentPubKey
+        tx2 =
+            Constraints.collectFromTheScript utxos signedPubKey
+            <> Constraints.mustIncludeDatumInTx unitDatum
+            <> Constraints.mustUseOutputAsCollateral utxoRefForBalance2
+            <> Constraints.mustSpendPubKeyOutput utxoRefForBalance2
+            <> Constraints.mustBeSignedBy signedPubKey
     logInfo @String $ "Required Signatories: " ++ show (Constraints.requiredSignatories tx2)
     submitTxConfirmed $ mkTx lookups2 tx2
 
