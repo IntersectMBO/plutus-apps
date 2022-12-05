9778bf66df9453afa5c7339fb5423302c4e617c0
Markus Läll <markus.lall@iohk.io>
2022-12-05 13:17:15 +0200

    Add suffixes to sqlite databases

--- marconi/src/Marconi/CLI.hs
+++ marconi/src/Marconi/CLI.hs
@@ -157,13 +157,13 @@ optAddressesParser :: Opt.Mod Opt.OptionFields [C.Address C.ShelleyAddr] -> Opt.
 optAddressesParser =  optional . multiString
 
 utxoDbName :: FilePath
-utxoDbName = "utxodb"
+utxoDbName = "utxo.db"
 
 datumDbName :: FilePath
-datumDbName = "datumdb"
+datumDbName = "datum.db"
 
 scriptTxDbName :: FilePath
-scriptTxDbName = "scripttxdb"
+scriptTxDbName = "scripttx.db"
 
 utxoDbPath :: Options -> Maybe FilePath
 utxoDbPath o = if optionsDisableUtxo o then Nothing; else Just (optionsDbPath o </> utxoDbName)
