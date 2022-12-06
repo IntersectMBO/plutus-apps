51edcad0149460f8a9bda1f459daf1f5976b027d
Parthiv Seetharaman <pachum99@myrdd.info>
2022-12-06 13:32:55 -0800

    fix(ci): pull ciJobs info from flake(not cloned)

--- nix/tullia.nix
+++ nix/tullia.nix
@@ -37,7 +37,7 @@ rec {
       in
       config.preset.github.status.lib.reportBulk {
         bulk.text = ''
-          nix eval .#ciJobs --apply __attrNames --json | # all systems the flake declares
+          nix eval ${flakeUrl}#ciJobs --apply __attrNames --json | # all systems the flake declares
           nix-systems -i | # figure out which the current machine is able to build
           jq 'with_entries(select(.value))' # only keep those we can build
         '';
