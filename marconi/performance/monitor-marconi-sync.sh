9017a8a7a87898eee5d4f870898e9f81009f8f50
James Browning <james.browning@iohk.io>
2022-12-16 15:51:08 +0000

    fix shellcheck issues

--- marconi/performance/monitor-marconi-sync.sh
+++ marconi/performance/monitor-marconi-sync.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# For running and monitoring sync testing marconi
+# For monitoring time to sync marconi. Logs cpu, mem and sync %.
 
+# Run marconi with "> >(tee marconi-log.txt)" and then run this script with marconi's log as arg
 if [[ $1 ]]; then marconi_log=$1; else echo "provide marconi log file (arg0)" && exit 1; fi
 
 startMs=$(date +%s)
@@ -16,23 +17,23 @@ else
 	echo -e "marconi pid=$pid\n"
 fi
 
-exec > >(tee log.txt)
+exec > >(tee monitor-marconi-log.txt)
 
 echo -e "Starting monitoring at $(date)"
 echo -e "Process started at $(ps -p "$pid" -o start_time=)\n"
 ps -p "$pid" -o etime,%cpu,rss,%mem
 
-while [ true ]
+while true
 do
-	last_log=$(echo $(tail -n1 $marconi_log))
-	sync_percent=$(echo $(echo $last_log | cut -d '(' -f2 | cut -d ')' -f1))
+	last_log=$(tail -n1 "$marconi_log")
+	sync_percent=$(echo "$last_log" | cut -d '(' -f2 | cut -d ')' -f1)
 	echo -e "Synced: $sync_percent"
 	ps -p "$pid" -o etime=,%cpu=,rss=,%mem=
 
-	if [[ "$last_log" == *"Synchronised"* ]] # check for contains "Synchronised"
+	if [[ "$last_log" == *"Synchronised"* ]]
 	then
 		echo -e "\nFinished at $(date)"
-		echo "Time taken: $(($(date +%s) - $startMs)) seconds"
+		echo "Time taken: $(($(date +%s) - startMs)) seconds"
 		exit 1
 	else
 		sleep 10
