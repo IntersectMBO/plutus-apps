88bf4c46d152110ad7989574b4786c7643e16efc
zeme-iohk <lorenzo.calegari@iohk.io>
2023-01-31 15:32:45 +0700

    Catch up to old nix

--- __std__/cells/plutus-apps/library/make-plutus-apps-project.nix
+++ __std__/cells/plutus-apps/library/make-plutus-apps-project.nix
@@ -24,7 +24,7 @@ let
       "https://github.com/input-output-hk/cardano-config"."1646e9167fab36c0bff82317743b96efa2d3adaa" = "sha256-TNbpnR7llUgBN2WY7CryMxNVupBIUH01h1hRNHoxboY=";
       "https://github.com/input-output-hk/cardano-ledger"."da3e9ae10cf9ef0b805a046c84745f06643583c2" = "sha256-3VUZKkLu1R43GUk9IwgsGQ55O0rnu8NrCkFX9gqA4ck=";
       "https://github.com/input-output-hk/cardano-wallet"."18a931648550246695c790578d4a55ee2f10463e" = "0i40hp1mdbljjcj4pn3n6zahblkb2jmpm8l4wnb36bya1pzf66fx";
-      "https://github.com/Quviq/quickcheck-contractmodel"."462f8c25bea15f88e179e12ebf4e3a4ffcc92d18" = "sha256-0NXBaWLGL5Hoz/A/yLIh5NZ4F5tqQUScB4Tr5u1Wg1Y=";
+      "https://github.com/Quviq/quickcheck-contractmodel"."5cd16ba995f1cd48c2b5f885c702ca6f2a2abbaa" = "sha256-rVVdr68f+xEOV4s27Ix+wdBznSVFvq+Q2ZBo0SEnkp0=";
       "https://github.com/sevanspowell/hw-aeson"."b5ef03a7d7443fcd6217ed88c335f0c411a05408" = "1dwx90wqavdl4d0npbzbxyh2pzi9zs1qz7nvsrb3n1cm2xbv4i5z";
     };
 
