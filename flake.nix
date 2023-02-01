79dbbfbae5fde0420ed30c74167b57b27c7d07a7
zeme-iohk <lorenzo.calegari@iohk.io>
2023-02-01 17:51:25 +0700

    HLS 1.8.0

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
       flake = false;
     };
     haskell-language-server = {
-      url = "github:haskell/haskell-language-server?ref=1.9.0.0";
+      url = "github:haskell/haskell-language-server?ref=1.8.0.0";
       flake = false;
     };
     iohk-nix = {
