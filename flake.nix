bc1c3fe6a673773a562604c7d5525ad918ad91ee
zeme-iohk <lorenzo.calegari@iohk.io>
2023-02-01 22:15:46 +0700

    WIP

--- flake.nix
+++ flake.nix
@@ -37,6 +37,7 @@
       flake = false;
     };
     haskell-language-server = {
+      # TODO Upgrade to 1.9.0.0 once plutus-apps hits GHC 9.2
       url = "github:haskell/haskell-language-server?ref=1.8.0.0";
       flake = false;
     };
