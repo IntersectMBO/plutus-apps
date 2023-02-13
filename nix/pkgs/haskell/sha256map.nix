d512ee8e2b3d57bb0daad8d0aff57686744cef30
Maximilian Algehed <m.algehed@gmail.com>
2023-02-13 15:29:40 +0100

    Update contractmodel dependency to latest version on master

--- nix/pkgs/haskell/sha256map.nix
+++ nix/pkgs/haskell/sha256map.nix
@@ -3,7 +3,7 @@
   "https://github.com/input-output-hk/cardano-config"."1646e9167fab36c0bff82317743b96efa2d3adaa" = "sha256-TNbpnR7llUgBN2WY7CryMxNVupBIUH01h1hRNHoxboY=";
   "https://github.com/input-output-hk/cardano-ledger"."da3e9ae10cf9ef0b805a046c84745f06643583c2" = "sha256-3VUZKkLu1R43GUk9IwgsGQ55O0rnu8NrCkFX9gqA4ck=";
   "https://github.com/input-output-hk/cardano-wallet"."18a931648550246695c790578d4a55ee2f10463e" = "0i40hp1mdbljjcj4pn3n6zahblkb2jmpm8l4wnb36bya1pzf66fx";
-  "https://github.com/Quviq/quickcheck-contractmodel"."5cd16ba995f1cd48c2b5f885c702ca6f2a2abbaa" = "sha256-rVVdr68f+xEOV4s27Ix+wdBznSVFvq+Q2ZBo0SEnkp0=";
+  "https://github.com/Quviq/quickcheck-contractmodel"."b66e3ff0c6011dcef63f2bcfa6ea8d6ab685230d" = "sha256-gnY/Ps9i7RqO3yjcFilXjHyqDsdSZ+sLQn4vu3mAK/A=";
   "https://github.com/sevanspowell/hw-aeson"."b5ef03a7d7443fcd6217ed88c335f0c411a05408" = "1dwx90wqavdl4d0npbzbxyh2pzi9zs1qz7nvsrb3n1cm2xbv4i5z";
   "https://github.com/james-iohk/cardano-node"."9e2426a89204b339d0cc6d848dba13719014fadc" = "00ax9dvr256kdr0c6i7kafhxra2hk7xywgl8k4wgq14wxv56gfzq";
   "https://github.com/james-iohk/cardano-node"."2b6978583b306ad28f7e1226b5d1ad604919c98a" = "06nggf0kpdh3whn6kqpw8s0484xw1q9r964rv125grbmpdx86dah";
