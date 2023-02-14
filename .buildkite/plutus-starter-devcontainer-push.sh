4e4d94739a0718c9a8591e36574027974282ff24
zeme-iohk <lorenzo.calegari@iohk.io>
2023-02-14 13:17:47 +0700

    Migrate script-evaluation-dump from BuildKite to GHA (#955)

--- .buildkite/plutus-starter-devcontainer-push.sh
+++ .buildkite/plutus-starter-devcontainer-push.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+# TODO(std) remove this after https://github.com/input-output-hk/plutus-apps/pull/951
 nix-build default.nix -A build-and-push-devcontainer-script -o build-and-push-image.sh
 ./build-and-push-image.sh
