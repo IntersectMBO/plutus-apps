5de9b2f2d35655341dd1203a24bf53e5ee5e6418
zeme-iohk <lorenzo.calegari@iohk.io>
2023-02-19 19:19:12 +0700

    Fix format

--- __std__/cells/plutus-apps/packages/haskell-language-server.nix
+++ __std__/cells/plutus-apps/packages/haskell-language-server.nix
@@ -2,15 +2,15 @@
 
 { inputs, cell }:
 
-let 
+let
   exes = cell.library.haskell-language-server-project.hsPkgs.haskell-language-server.components.exes; # editorconfig-checker-disable-line
-in  
-  cell.library.pkgs.stdenv.mkDerivation {
-    name = "haskell-language-server";
-    phases = [ "installPhase" ];
-    installPhase = ''
-      mkdir -p $out/bin
-      cp -r ${exes.haskell-language-server}/bin/* $out/bin
-      cp -r ${exes.haskell-language-server-wrapper}/bin/* $out/bin
-    '';
-  }
+in
+cell.library.pkgs.stdenv.mkDerivation {
+  name = "haskell-language-server";
+  phases = [ "installPhase" ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r ${exes.haskell-language-server}/bin/* $out/bin
+    cp -r ${exes.haskell-language-server-wrapper}/bin/* $out/bin
+  '';
+}
