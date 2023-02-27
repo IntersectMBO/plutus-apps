b497fb477f56d0912640d76693ac135fc6aa35ec
Sjoerd Visscher <sjoerd.visscher@iohk.io>
2023-02-27 12:56:15 +0100

    Log TxIn witness info

--- plutus-ledger/src/Ledger/Tx/Internal.hs
+++ plutus-ledger/src/Ledger/Tx/Internal.hs
@@ -86,10 +86,12 @@ instance Pretty TxIn where
     pretty TxIn{txInRef,txInType} =
                 let rest =
                         case txInType of
-                            Just (ScriptAddress _ redeemer _) ->
-                                pretty redeemer
+                            Just (ScriptAddress wit redeemer md) ->
+                                [ either (\v -> "Validator:" <+> pretty v) (\ref -> "Script reference:" <+> pretty ref) wit
+                                , "Redeemer:" <+> pretty redeemer]
+                                ++ maybe [] (\d -> ["Datum:" <+> pretty d]) md
                             _ -> mempty
-                in hang 2 $ vsep ["-" <+> pretty txInRef, rest]
+                in hang 2 $ vsep $ ("-" <+> pretty txInRef) : rest
 
 -- | A transaction input that spends a "pay to public key" output, given the witness.
 pubKeyTxIn :: TxOutRef -> TxIn
