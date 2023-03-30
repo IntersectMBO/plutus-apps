9eb6417e4f424b34d70d6b3649afb9431846e872
Markus Läll <markus.l2ll@gmail.com>
2023-03-30 16:20:12 +0300

    Add helper to query security param from the node

--- marconi-chain-index/src/Marconi/ChainIndex/Utils.hs
+++ marconi-chain-index/src/Marconi/ChainIndex/Utils.hs
@@ -1,9 +1,12 @@
 module Marconi.ChainIndex.Utils
     ( isBlockRollbackable
+    , querySecurityParam
     ) where
 
 import Cardano.Api qualified as C
-import Data.Word (Word64)
+import Cardano.Streaming.Helpers qualified as C
+import Control.Monad.IO.Class (liftIO)
+import Ouroboros.Consensus.HardFork.Combinator.AcrossEras (EraMismatch)
 
 isBlockRollbackable :: Int -> C.BlockNo -> C.ChainTip -> Bool
 isBlockRollbackable securityParam (C.BlockNo chainSyncBlockNo) localChainTip =
@@ -13,3 +16,24 @@ isBlockRollbackable securityParam (C.BlockNo chainSyncBlockNo) localChainTip =
               (C.ChainTip _ _ (C.BlockNo bn)) -> bn
      -- TODO Need to confirm if it's "<" or "<="
      in chainTipBlockNo - chainSyncBlockNo <= toEnum securityParam
+
+-- | Query security param from node
+querySecurityParam :: C.NetworkId -> FilePath -> IO Int
+querySecurityParam networkId socketPath = do
+  result <- liftIO $ C.queryNodeLocalState localNodeConnectInfo Nothing queryInMode
+  return $ C.protocolParamSecurity $ either showError (either showError id) result
+
+  where
+    localNodeConnectInfo :: C.LocalNodeConnectInfo C.CardanoMode
+    localNodeConnectInfo = C.mkLocalNodeConnectInfo networkId socketPath
+
+    eraInMode :: C.EraInMode C.BabbageEra C.CardanoMode
+    eraInMode = C.BabbageEraInCardanoMode
+
+    queryInMode :: C.QueryInMode C.CardanoMode (Either EraMismatch C.GenesisParameters)
+    queryInMode = C.QueryInEra eraInMode
+      $ C.QueryInShelleyBasedEra (C.shelleyBasedEra @C.BabbageEra) C.QueryGenesisParameters
+
+    -- TODO /Really/ handle the error.
+    showError :: Show a => a -> b
+    showError = error . show
