dbdbcbe608797fcda0aacc35522a4e6744dfd62a
zeme-iohk <lorenzo.calegari@iohk.io>
2023-04-11 17:21:48 +0700

    Bring back doc/adr/0018-Indexers-query-synchronisation-primitive (#1052)

--- __std__/cells/plutus-apps/packages/sphinx-build-readthedocs-site.nix
+++ __std__/cells/plutus-apps/packages/sphinx-build-readthedocs-site.nix
@@ -9,7 +9,6 @@ cell.library.pkgs.writeShellApplication {
     cell.packages.sphinx-toolchain
   ];
   text = ''
-    export SPHINX_HADDOCK_DIR="${cell.packages.combined-plutus-apps-haddock}/share/doc"
     root="$(repo-root)"
     sphinx-build -j 4 -n "$root/doc" "$root/doc/_build"
   '';
