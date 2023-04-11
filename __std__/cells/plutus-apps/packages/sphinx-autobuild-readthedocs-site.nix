45203bae97d3ed1a9272565370f06284602ef912
zeme-iohk <lorenzo.calegari@iohk.io>
2023-04-11 08:34:42 +0700

    UPD

--- __std__/cells/plutus-apps/packages/sphinx-autobuild-readthedocs-site.nix
+++ __std__/cells/plutus-apps/packages/sphinx-autobuild-readthedocs-site.nix
@@ -9,7 +9,6 @@ cell.library.pkgs.writeShellApplication {
     cell.packages.sphinx-toolchain
   ];
   text = ''
-    export SPHINX_HADDOCK_DIR="${cell.packages.combined-plutus-apps-haddock}/share/doc"
     root="$(repo-root)"
     sphinx-autobuild -j 4 -n "$root/doc" "$root/doc/_build"
   '';
