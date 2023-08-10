9137ce05cb20bc5bd40ab87227a449822c5c530a
zeme <lorenzo.calegari@iohk.io>
2023-08-10 12:02:04 +0200

    shellcheck

--- plutus-pab/local-cluster/cluster-data/cardano-node-shelley/start.sh
+++ plutus-pab/local-cluster/cluster-data/cardano-node-shelley/start.sh
@@ -14,17 +14,17 @@ config_dir=lib/shelley/test/data/cardano-node-shelley
 
 mkdir -p "${state_dir:=bft-node}"
 
-yq -y '. + { GenesisFile: "genesis.json", minSeverity: "Info" }' < $config_dir/node.config > $state_dir/node.config
+yq -y '. + { GenesisFile: "genesis.json", minSeverity: "Info" }' < "$config_dir/node.config" > "$state_dir/node.config"
 
-yq ".systemStart=\"$systemStart\"" < $config_dir/genesis.yaml > $state_dir/genesis.json
+yq ".systemStart=\"$systemStart\"" < "$config_dir/genesis.yaml" > "$state_dir/genesis.json"
 
 set -x
 
 exec cardano-node run --port 40000 \
-     --config $state_dir/node.config \
+     --config "$state_dir/node.config" \
      --topology lib/byron/test/data/cardano-node-byron/node.topology \
-     --database-path $state_dir/node.db \
-     --socket-path $state_dir/node.socket \
-     --shelley-vrf-key $config_dir/node-vrf.skey \
-     --shelley-kes-key $config_dir/node-kes.skey \
-     --shelley-operational-certificate $config_dir/node.opcert
+     --database-path "$state_dir/node.db" \
+     --socket-path "$state_dir/node.socket" \
+     --shelley-vrf-key "$config_dir/node-vrf.skey" \
+     --shelley-kes-key "$config_dir/node-kes.skey" \
+     --shelley-operational-certificate "$config_dir/node.opcert"
